8e72d0f952665e915df82cf967160cf56ea5c089
Rasmus <scratty@gmail.com>
2023-11-30 14:26:55 +0100

    Adding new header file Delay.h that is platform independent. Probably not necessary to separate the methods but reassuring for now.

---
+++ Delay.h
@@ -0,0 +1,29 @@
+//
+//  Delay.h
+//  Terminal Text Lib
+//
+//  Created by Rasmus Anthin on 2023-11-30.
+//
+
+#pragma once
+#ifdef _WIN32
+#include <thread>
+#else
+#include <unistd.h>
+#endif
+
+
+namespace Delay
+{
+  // Arguments:
+  // int us : microseconds.
+  // #FIXME: Choose one.
+  void sleep(int us)
+  {
+#ifdef _WIN32
+    std::this_thread::sleep_for(std::chrono::microseconds(us));
+#else
+    usleep(us);
+#endif
+  }
+}
