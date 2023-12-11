a2b6e8ec09e47252cd014f27762fa6936fe74c5c
Rasmus <scratty@gmail.com>
2023-12-11 13:23:56 +0100

    Simplifying Delay::sleep() so that it uses std::this_thread::sleep_for() for all platforms (win, mac, linux) instead of using usleep() on just mac and linux.

--- Delay.h
+++ Delay.h
@@ -6,24 +6,15 @@
 //
 
 #pragma once
-#ifdef _WIN32
 #include <thread>
-#else
-#include <unistd.h>
-#endif
 
 
 namespace Delay
 {
   // Arguments:
   // int us : microseconds.
-  // #FIXME: Choose one for all platforms if possible.
   void sleep(int us)
   {
-#ifdef _WIN32
     std::this_thread::sleep_for(std::chrono::microseconds(us));
-#else
-    usleep(us);
-#endif
   }
 }
