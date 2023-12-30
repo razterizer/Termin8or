1cb22880e14e2c7e879d2c3a8f385ced5a156b58
Rasmus <scratty@gmail.com>
2023-12-30 16:52:40 +0100

    Adding new function Delay::update_loop() that is a great upgrade to the Delay::sleep() function in that it sleeps the remaining time that is left of the current real-time frame. This is a game changer that makes the games much more snappy.

--- Delay.h
+++ Delay.h
@@ -7,6 +7,7 @@
 
 #pragma once
 #include <thread>
+#include <functional>
 
 
 namespace Delay
@@ -16,5 +17,32 @@ namespace Delay
   void sleep(int us)
   {
     std::this_thread::sleep_for(std::chrono::microseconds(us));
+  }
+
+  void update_loop(int fps, std::function<bool(void)> update_func)
+  {
+    std::chrono::milliseconds frame_time(1000 / fps);
+    auto last_time = std::chrono::steady_clock::now();
+
+    while (true)
+    {
+      if (!update_func())
+        return;
+
+      // Check time elapsed since the last frame.
+      auto current_time = std::chrono::steady_clock::now();
+      auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_time);
+
+      if (elapsed_time < frame_time)
+      {
+        // Sleep to limit the refresh rate.
+        std::this_thread::sleep_for(frame_time - elapsed_time);
+      }
+
+      //refresh();
+      //Delay::sleep(delay);
+
+      last_time = current_time;
+    }
   }
 }
