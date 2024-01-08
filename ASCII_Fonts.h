7048e5f5f7cf73da329ed1b26a960c6623281b1c
Rasmus <scratty@gmail.com>
2024-01-08 11:44:57 +0100

    ASCII_Fonts.h: * Changing glyph ordering method from absolute character priorities to pair-wise characters (substrings of size 2), like in a kerning-table. This method is much more versatile and robust.

--- ASCII_Fonts.h
+++ ASCII_Fonts.h
@@ -55,13 +55,13 @@ namespace ASCII_Fonts
   {
     std::vector<FontPiece> font_pieces;
     int width = -1;
-    int priority = -1;
   };
   
   struct FontData
   {
     std::map<char, FontChar> font_chars;
     std::map<std::pair<char, char>, int> kernings;
+    std::map<std::pair<char, char>, std::pair<int, int>> orderings;
   };
 
   using FontDataColl = std::map<Font, FontData>;
@@ -174,13 +174,17 @@ namespace ASCII_Fonts
       char kch0 = -1;
       char kch1 = -1;
       int kern = 0;
+      char och0 = -1;
+      char och1 = -1;
+      int opri0 = 0;
+      int opri1 = 0;
       int width = -1;
-      int priority = -1; // Higher number means higher priority.
       FontPiece piece;
       int r, c;
       char fg[4];
       char bg[4];
       bool kerning = false;
+      bool ordering = false;
       
       if (txt_file.is_open())
       {
@@ -206,22 +210,35 @@ namespace ASCII_Fonts
         std::string line;
         while (std::getline(txt_file, line))
         {
-          if (sscanf(line.c_str(), "char: '%c', width: %i, priority: %i", &ch, &width, &priority) == 3)
+          // Data section.
+          if (sscanf(line.c_str(), "char: '%c', width: %i", &ch, &width) == 2)
+          {
             kerning = false;
+            ordering = false;
+          }
           if (strcmp(line.c_str(), "kerning:") == 0)
           {
             kerning = true;
+            ordering = false;
             ch = -1;
           }
+          if (strcmp(line.c_str(), "ordering:") == 0)
+          {
+            kerning = false;
+            ordering = true;
+            ch = -1;
+          }
+          
+          // Read data.
           if (kerning && sscanf(line.c_str(), "'%c' -> '%c' = %i", &kch0, &kch1, &kern) == 3)
             curr_font.kernings[{kch0, kch1}] = kern;
+          else if (ordering && sscanf(line.c_str(), "'%c' -> '%c' = %i, %i", &och0, &och1, &opri0, &opri1) == 4)
+            curr_font.orderings[{och0, och1}] = {opri0, opri1};
           else if (ch != -1)
           {
             auto& curr_char = curr_font.font_chars[ch];
             if (curr_char.width == -1)
               curr_char.width = width;
-            if (curr_char.priority == -1)
-              curr_char.priority = priority;
             if (line.size() > 0 && line[0] == '"')
             {
               piece.part = line.substr(1, line.size() - 2);
@@ -246,49 +263,82 @@ namespace ASCII_Fonts
   // (r, c) : top left corner of text.
   // returns the relative start column (top left corner) for the next character.
   template<int NR, int NC>
-  int draw_char(SpriteHandler<NR, NC>& sh, const FontDataColl& font_data, const char ch_curr, const char ch_next,
-                int r, int c, Font font, int custom_kerning = 0)
+  int draw_char(SpriteHandler<NR, NC>& sh, const FontData& curr_font, const char ch_curr, const char ch_next,
+                int ch_curr_order,
+                int r, int c,
+                int custom_kerning = 0)
   {
-    auto it_font = font_data.find(font);
-    if (it_font != font_data.end())
+    auto it_char = curr_font.font_chars.find(ch_curr);
+    if (it_char != curr_font.font_chars.end())
     {
-      const auto& curr_font = it_font->second;
+      const auto& curr_char = it_char->second;
+      for (const auto& piece : curr_char.font_pieces)
+      {
+        // #FIXME: Glyphs should be ordered depending on sequence and character. Like the kerning. Perhaps add a separate section for such orderings. E.g. 'A' -> 'A' : 0, 1. Meaning the second glyph will be drawn on top of the first glyph. This may require a tree in order to sort the characters by global priority.
+        OrderedText t;
+        t.str = piece.part;
+        t.r = r + piece.r;
+        t.c = c + piece.c;
+        t.fg_color = piece.fg_color;
+        t.bg_color = piece.bg_color;
+        //t.priority = curr_char.priority;
+        t.priority = ch_curr_order;
+        sh.add_ordered_text(t);
+      }
+        
+      int kerning = 0;
+      if (ch_next != -1)
+      {
+        auto it_k = curr_font.kernings.find({ch_curr, ch_next});
+        if (it_k != curr_font.kernings.end())
+          kerning = it_k->second + custom_kerning;
+      }
       
-      auto it_char = curr_font.font_chars.find(ch_curr);
-      if (it_char != curr_font.font_chars.end())
-      {
-        const auto& curr_char = it_char->second;
-        for (const auto& piece : curr_char.font_pieces)
-        {
-          // #FIXME: Glyphs should be ordered depending on sequence and character. Like the kerning. Perhaps add a separate section for such orderings. E.g. 'A' -> 'A' : 0, 1. Meaning the second glyph will be drawn on top of the first glyph. This may require a tree in order to sort the characters by global priority.
-          OrderedText t;
-          t.str = piece.part;
-          t.r = r + piece.r;
-          t.c = c + piece.c;
-          t.fg_color = piece.fg_color;
-          t.bg_color = piece.bg_color;
-          t.priority = curr_char.priority;
-          sh.add_ordered_text(t);
-        }
-        
-        int kerning = 0;
-        if (ch_next != -1)
-        {
-          auto it_k = curr_font.kernings.find({ch_curr, ch_next});
-          if (it_k != curr_font.kernings.end())
-            kerning = it_k->second + custom_kerning;
-        }
-        
-        return curr_char.width + kerning;
-      }
-      else
-      {
-        std::cerr << "Warning: Unrecognized character '" << ch_curr << "'" << std::endl;
-      }
+      return curr_char.width + kerning;
+    }
+    else
+    {
+      std::cerr << "Warning: Unrecognized character '" << ch_curr << "'" << std::endl;
     }
     
     return 0;
   }
+  
+  
+  std::vector<std::pair<char, int>> sort_text(const std::string& text, const FontData& curr_font)
+  {
+    if (text.empty())
+      return {};
+      
+    std::vector<std::pair<char, int>> ordered_text;
+    ordered_text.emplace_back(text[0], 0);
+    int curr_prio = 0;
+    int prev_prio = 0;
+    int num_chars = static_cast<int>(text.size());
+    for (int ch_idx = 1; ch_idx < num_chars; ++ch_idx)
+    {
+      char ch_curr = text[ch_idx];
+      char ch_prev = text[ch_idx - 1];
+      
+      auto it_o = curr_font.orderings.find({ ch_prev, ch_curr });
+      if (it_o != curr_font.orderings.end())
+      {
+        auto [o0, o1] = it_o->second;
+        if (o0 < o1)
+          curr_prio = prev_prio + 1;
+        else if (o0 < o1)
+          curr_prio = prev_prio - 1;
+        else
+          curr_prio = prev_prio;
+      }
+      else
+        curr_prio = prev_prio;
+        
+      ordered_text.emplace_back(ch_curr, curr_prio);
+    }
+    return ordered_text;
+  }
+  
 
   // (r, c) : top left corner of text.
   template<int NR, int NC>
@@ -298,17 +348,29 @@ namespace ASCII_Fonts
     int width = 0;
     int num_chars = static_cast<int>(text.size());
     int num_custom_kernings = static_cast<int>(custom_kerning.size());
-    for (int ch_idx = 0; ch_idx < num_chars; ++ch_idx)
+    auto it_font = font_data.find(font);
+    if (it_font != font_data.end())
     {
-      char ch_curr = text[ch_idx];
-      char ch_next = ch_idx + 1 < num_chars ? text[ch_idx + 1] : -1;
-      int ck = 0;
-      if (ch_idx < num_custom_kernings)
-        ck = custom_kerning[ch_idx];
-      width += draw_char(sh, font_data, ch_curr, ch_next, r, c + width, font, ck);
-    }
+      const auto& curr_font = it_font->second;
+      
+      auto ordered_text = sort_text(text, curr_font);
+      for (int ch_idx = 0; ch_idx < num_chars; ++ch_idx)
+      {
+        char ch_curr = text[ch_idx];
+        char ch_next = ch_idx + 1 < num_chars ? text[ch_idx + 1] : -1;
+        int ck = 0;
+        if (ch_idx < num_custom_kernings)
+          ck = custom_kerning[ch_idx];
+        int ch_curr_order = ordered_text[ch_idx].second;
+        width += draw_char(sh, curr_font,
+          ch_curr, ch_next,
+          ch_curr_order,
+          r, c + width,
+          ck);
+      }
     
-    sh.write_buffer_ordered();
+      sh.write_buffer_ordered();
+    }
   }
 
 }
