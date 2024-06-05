2a9f9486613a7edd3bfca9d89f93036e4ab7235c
Rasmus Anthin <scratty@gmail.com>
2024-06-05 22:14:21 +0200

    GameEngine.h: * Added include to FolderHelper.h since we need it for loading and saving the hiscores. See items below. * Added new private variable path_to_exe so that we know where to store data files such as the hiscore.txt file, i.e. next to the executable, as the pwd is not always where the exe is (especially not on Macos at least). * Added private variables hiscore_list, curr_score_item and hiscore_caret_idx. * Refactoring score variable by moving it from Pilot_Episode and SurgSim_Lite to this class as a private member, instead we can access it from our game code using the function ref_score(). * Added function handle_hiscores() that loads hiscores from file, merges with the current score after "game over" or "you won", then sorts them and finally stores them back to file again. * Added new callback functions which allow the game designer to e.g. start and stop music for different sections or what not:   - on_enter_game_over().   - on_exit_game_over().   - on_enter_you_won().   - on_exit_you_won().   - on_enter_input_hiscore().   - on_exit_input_hiscore().   - on_enter_hiscores(). * Added additional argument to constructor (front); exe_path which is to be taken from the argv[0] of the program. * Moving "game over" and "you won" screen rendering code from Pilot_Episode and SurgSim_Lite to here and thus you need to instead use the newly created functions set_state_game_over() and set_state_you_won() in your game to trigger either screen. * Making sure we don't use the quit confirm screen while showing the hiscores as it is sufficient with just a press on the space-bar to quit the program. * State machinery for "game over", "you won", input hiscore and hiscore list screens.

--- GameEngine.h
+++ GameEngine.h
@@ -11,6 +11,7 @@
 #include <Core/Delay.h>
 #include <Core/Rand.h>
 #include <Core/Math.h>
+#include <Core/FolderHelper.h>
 
 template<int NR = 30, int NC = 80>
 class GameEngine
@@ -24,6 +25,7 @@ class GameEngine
   bool show_input_hiscore = false;
   bool show_hiscores = false;
   
+  std::string_view path_to_exe;
   const bool use_wasd_arrow_keys = false;
   const Text::Color c_bg_color_default = Text::Color::Default;
   const Text::Color c_bg_color_title = Text::Color::Default;
@@ -33,6 +35,66 @@ class GameEngine
   int fps = 12; // 5
   
   YesNoButtons quit_confirm_button = YesNoButtons::No;
+  
+  std::vector<HiScoreItem> hiscore_list;
+  int score = 0;
+  HiScoreItem curr_score_item;
+  int hiscore_caret_idx = 0;
+  
+  bool handle_hiscores(const HiScoreItem& curr_hsi)
+  {
+    const int c_max_num_hiscores = 20;
+    const auto [exe_path, exe_file] = folder::split_file_path(std::string(path_to_exe));
+    const std::string c_file_path = folder::join_file_path({ exe_path, "hiscores.txt" });
+  
+    // Read saved hiscores.
+    std::vector<std::string> lines;
+    if (std::filesystem::exists(c_file_path))
+    {
+      if (!TextIO::read_file(c_file_path, lines))
+        return false;
+    }
+    
+    // Import saved hiscores from vector of strings.
+    hiscore_list.clear();
+    for (const auto& hs_str : lines)
+    {
+      std::istringstream iss(hs_str);
+      HiScoreItem hsi;
+      iss >> hsi.name >> hsi.score;
+      hiscore_list.emplace_back(hsi);
+    }
+    
+    // Add current hiscore.
+    hiscore_list.push_back(curr_hsi);
+    
+    // Sort hiscores.
+    auto num_hiscores = static_cast<int>(hiscore_list.size());
+    stlutils::sort(hiscore_list,
+      [](const auto& hsi_A, const auto& hsi_B) { return hsi_A.score < hsi_B.score; });
+    if (num_hiscores >= 1)
+      hiscore_list = stlutils::subset(hiscore_list, 0, std::min(num_hiscores, c_max_num_hiscores) - 1);
+    else
+    {
+      std::cerr << "ERROR: Unknown error while saving hiscores!" << std::endl;
+      return false;
+    }
+    
+    // Export hiscores to vector of strings.
+    lines.clear();
+    for (const auto& hsi : hiscore_list)
+    {
+      std::ostringstream oss;
+      oss << str::trim_ret(hsi.name) << " " << hsi.score;
+      lines.emplace_back(oss.str());
+    }
+    
+    // Save hiscores.
+    if (!TextIO::write_file(c_file_path, lines))
+      return false;
+    
+    return true;
+  }
   
 protected:
   float dt = static_cast<float>(delay) / 1e6f;
@@ -54,20 +116,31 @@ protected:
     dt = static_cast<float>(delay) / 1e6f;
   }
   
+  int& ref_score() { return score; }
+  
   // Callbacks
   virtual void update() = 0;
-  virtual void on_quit() {}
   virtual void draw_title() = 0;
   virtual void draw_instructions() = 0;
+  virtual void on_quit() {}
   virtual void on_exit_title() {}
   virtual void on_exit_instructions() {}
+  virtual void on_enter_game_over() {}
+  virtual void on_exit_game_over() {}
+  virtual void on_enter_you_won() {}
+  virtual void on_exit_you_won() {}
+  virtual void on_enter_input_hiscore() {}
+  virtual void on_exit_input_hiscore() {}
+  virtual void on_enter_hiscores() {}
   
 public:
-  GameEngine(bool use_wasd_keys,
+  GameEngine(std::string_view exe_path,
+             bool use_wasd_keys,
              const Text::Color bg_col_def,
              const Text::Color bg_col_title,
              const Text::Color bg_col_instr)
-    : use_wasd_arrow_keys(use_wasd_keys)
+    : path_to_exe(exe_path)
+    , use_wasd_arrow_keys(use_wasd_keys)
     , c_bg_color_default(bg_col_def)
     , c_bg_color_title(bg_col_title)
     , c_bg_color_instructions(bg_col_instr)
@@ -99,6 +172,9 @@ public:
   int get_fps() const { return fps; }
   int get_delay_us() const { return delay; }
   
+  void set_state_game_over() { show_game_over = true; }
+  void set_state_you_won() { show_you_won = true; }
+  
 private:
   bool engine_update()
   {
@@ -114,7 +190,7 @@ private:
     else if (kpd.pause)
       math::toggle(paused);
       
-    if (show_quit_confirm)
+    if (show_quit_confirm && !show_hiscores)
     {
       draw_confirm_quit(sh, quit_confirm_button);
       if (kpd.curr_special_key == keyboard::SpecialKey::Left)
@@ -160,6 +236,77 @@ private:
       }
       else if (paused)
         draw_paused(sh, anim_ctr);
+      else if (show_game_over)
+      {
+        if (game_over_timer == 0)
+        {
+          draw_game_over(sh);
+          update();
+        }
+        else
+        {
+          game_over_timer--;
+          if (game_over_timer == 0)
+            on_enter_game_over();
+        }
+          
+        if (kpd.curr_key == ' ')
+        {
+          on_exit_game_over();
+          show_game_over = false;
+          show_input_hiscore = true;
+          curr_score_item.reset(score, 5);
+          hiscore_caret_idx = 0;
+          on_enter_input_hiscore();
+        }
+      }
+      else if (show_you_won)
+      {
+        if (you_won_timer == 0)
+        {
+          draw_you_won(sh);
+          update();
+        }
+        else
+        {
+          you_won_timer--;
+          if (you_won_timer == 0)
+            on_enter_you_won();
+        }
+        
+        if (kpd.curr_key == ' ')
+        {
+          on_exit_you_won();
+          show_you_won = false;
+          show_input_hiscore = true;
+          curr_score_item.reset(score, 5);
+          hiscore_caret_idx = 0;
+          on_enter_input_hiscore();
+        }
+      }
+      else if (show_input_hiscore)
+      {
+        if (draw_input_hiscore(sh, kpd, curr_score_item, hiscore_caret_idx))
+        {
+          std::cout << "hiscore name: " << curr_score_item.name << std::endl;
+          on_exit_input_hiscore();
+          handle_hiscores(curr_score_item);
+          show_input_hiscore = false;
+          show_hiscores = true;
+          on_enter_hiscores();
+        }
+      }
+      else if (show_hiscores)
+      {
+        draw_hiscores(sh, hiscore_list);
+        
+        if (kpd.curr_key == ' ')
+        {
+          restore_cursor();
+          on_quit();
+          return false;
+        }
+      }
       else
         update();
     }
