0c0f9d4ca3e83a91324da6509c2f41dd83999475
Rasmus Anthin <scratty@gmail.com>
2024-06-09 20:18:37 +0200

    Rectangle.h: * Changing the data format for the members of Rectangle so that it doesn't mix x,y,width,height with r,c,r_len,c_len which it did before and it made it confusing to use.

--- Rectangle.h
+++ Rectangle.h
@@ -5,18 +5,18 @@ namespace ttl
   
   struct Rectangle
   {
-    Rectangle(int r, int c, int w, int h)
-      : x(c), y(r), width(w), height(h)
+    Rectangle2(int rr, int cc, int rr_len, int cc_len)
+      : r(rr), c(cc), r_len(rr_len), c_len(cc_len)
     {}
+    
+    int r = 0;
+    int c = 0;
+    int r_len = 0; // #NOTE: Downwards height!
+    int c_len = 0;
   
-    int x = 0;
-    int y = 0;
-    int width = 0;
-    int height = 0; // #NOTE: Downwards height!
-  
-    bool is_inside(int r, int c)
+    bool is_inside(int rr, int cc)
     {
-      return y <= r && r <= y + height && x <= c && c <= x + width;
+      return r <= rr && rr <= r + r_len && c <= cc && cc <= c + c_len;
     }
   };
   
