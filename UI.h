f6ae8544be28d6ad8995c7cf6a53c1ea15cfe195
Rasmus Anthin <scratty@gmail.com>
2024-07-08 19:54:51 +0200

    UI.h: * Added two new enum classes VerticalAlignment and HorizontalAlignment. * Removed obsolete class TextBoxCentered. * Added function overload draw() in TextBox that allows you to align the textbox in different portions of the SpriteHandler screen.

--- UI.h
+++ UI.h
@@ -11,6 +11,9 @@
 namespace ui
 {
 
+  enum class VerticalAlignment { TOP, CENTER, BOTTOM };
+  enum class HorizontalAlignment { LEFT, CENTER, RIGHT };
+
   class TextBox
   {
     str::StringBox sb;
@@ -66,11 +69,40 @@ namespace ui
       if (draw_box_bkg)
         drawing::draw_box(sh, pos.r - 1 - box_padding_ud, pos.c - 1 - box_padding_lr, N + 1 + 2*box_padding_ud, len_max + 1 + 2*box_padding_lr, box_style);
     }
-  };
-  
-  class TextBoxCentered : TextBox
-  {
-  
+    
+    template<int NR, int NC>
+    void draw(SpriteHandler<NR, NC>& sh, VerticalAlignment v_align, HorizontalAlignment h_align, const styles::Style& style, bool draw_box_outline, bool draw_box_bkg, int box_padding_lr = 0, int box_padding_ud = 0, std::optional<styles::Style> box_outline_style = std::nullopt, drawing::OutlineType outline_type = drawing::OutlineType::Line)
+    {
+      RC pos { 0, 0 };
+      
+      switch (v_align)
+      {
+        case VerticalAlignment::TOP:
+          pos.r = 0;
+          break;
+        case VerticalAlignment::CENTER:
+          pos.r = std::round((NR - N)*0.5f);
+          break;
+        case VerticalAlignment::BOTTOM:
+          pos.r = NR - N;
+          break;
+      }
+      
+      switch (h_align)
+      {
+        case HorizontalAlignment::LEFT:
+          pos.c = 0;
+          break;
+        case HorizontalAlignment::CENTER:
+          pos.c = std::round((NC - len_max)*0.5f);
+          break;
+        case HorizontalAlignment::RIGHT:
+          pos.c = NC - len_max;
+          break;
+      }
+      
+      draw(sh, pos, style, draw_box_outline, draw_box_bkg, box_padding_lr, box_padding_ud, box_outline_style, outline_type);
+    }
   };
   
   class TextBoxDebug : TextBox
