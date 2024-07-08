81817f89f427d5c21fd4f74921f943ef2239fcdc
Rasmus Anthin <scratty@gmail.com>
2024-07-08 15:15:30 +0200

    UI.h: * Now TextBox::draw() properly draws the box bkg and/or border (or here outline).

--- UI.h
+++ UI.h
@@ -44,10 +44,14 @@ namespace ui
     }
     
     template<int NR, int NC>
-    void draw(SpriteHandler<NR, NC>& sh, const RC& pos, const styles::Style& style, bool draw_outline, bool draw_bkg, int box_padding = 0)
+    void draw(SpriteHandler<NR, NC>& sh, const RC& pos, const styles::Style& box_style, bool draw_box_outline, bool draw_box_bkg, int box_padding_lr = 0, int box_padding_ud = 0, std::optional<styles::Style> box_outline_style = std::nullopt, drawing::OutlineType outline_type = drawing::OutlineType::Line)
     {
       for (size_t l_idx = 0; l_idx < N; ++l_idx)
-        sh.write_buffer(sb[l_idx], pos.r + l_idx, pos.c, style);
+        sh.write_buffer(sb[l_idx], pos.r + l_idx, pos.c, box_style);
+      if (draw_box_outline)
+        drawing::draw_box_outline(sh, pos.r - 1 - box_padding_ud, pos.c - 1 - box_padding_lr, N + 1 + 2*box_padding_ud, len_max + 1 + 2*box_padding_lr, outline_type, box_outline_style.value_or(box_style));
+      if (draw_box_bkg)
+        drawing::draw_box(sh, pos.r - 1 - box_padding_ud, pos.c - 1 - box_padding_lr, N + 1 + 2*box_padding_ud, len_max + 1 + 2*box_padding_lr, box_style);
     }
   };
   
