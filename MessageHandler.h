7c804f936e9320d790c8c5c52a75a5c164babc2a
Rasmus Anthin <scratty@gmail.com>
2024-08-05 09:12:08 +0200

    MessageHandler.h: * Fixing the order of the messages so that it follows a FIFO scheme while preserving order of severity (highest severity first). * Adding function clear_curr_message().

--- MessageHandler.h
+++ MessageHandler.h
@@ -9,6 +9,7 @@
 #include "Color.h"
 #include "UI.h"
 #include <execution>
+#include <queue>
 
 class MessageHandler
 {
@@ -16,14 +17,16 @@ public:
   enum class Level { Guide, Warning, Fatal };
   
 private:
-  const float duration = 1.5f; //4.f;
   float trg_time = 0.f;
   bool message_empty = true;
-  std::vector<std::pair<std::string, Level>> messages;
+  std::queue<std::pair<std::string, float>> messages_guide;
+  std::queue<std::pair<std::string, float>> messages_warning;
+  std::queue<std::pair<std::string, float>> messages_fatal;
   std::string curr_message;
+  Level curr_level;
+  float curr_duration_s = 1.5f;
   ui::TextBox tb;
   int str_len = 0;
-  Level curr_level;
   
   Color get_fg_color() const
   {
@@ -47,27 +50,66 @@ private:
     }
   }
   
-public:
-  void add_message(float time, const std::string& msg, Level lvl)
+  bool has_messages() const
   {
-    messages.emplace_back(msg, lvl);
-    std::sort(messages.begin(), messages.end(),
-      [](const auto& msgA, const auto& msgB) { return msgA.second < msgB.second;} );
+    return !messages_guide.empty() || !messages_warning.empty() || !messages_fatal.empty();
+  }
+  
+  std::tuple<std::string, Level, float> fetch_next_message()
+  {
+    if (!messages_fatal.empty())
+    {
+      const auto& msg = messages_fatal.front();
+      const auto ret = std::make_tuple(msg.first, Level::Fatal, msg.second);
+      messages_fatal.pop();
+      return ret;
+    }
+    if (!messages_warning.empty())
+    {
+      const auto& msg = messages_warning.front();
+      const auto ret = std::make_tuple(msg.first, Level::Warning, msg.second);
+      messages_warning.pop();
+      return ret;
+    }
+    if (!messages_guide.empty())
+    {
+      const auto& msg = messages_guide.front();
+      const auto ret = std::make_tuple(msg.first, Level::Guide, msg.second);
+      messages_guide.pop();
+      return ret;
+    }
+    return { "<Invalid Message>", Level::Guide, 0.5f };
+  }
+  
+public:
+  void add_message(float time, const std::string& msg, Level lvl, float duration_s = 1.5f)
+  {
+    switch (lvl)
+    {
+      case Level::Guide:
+        messages_guide.push({ msg, duration_s });
+        break;
+      case Level::Warning:
+        messages_warning.push({ msg, duration_s });
+        break;
+      case Level::Fatal:
+        messages_fatal.push({ msg, duration_s });
+        break;
+    }
   }
   
   template<int NR, int NC>
   void update(SpriteHandler<NR, NC>& sh, float time, ui::VerticalAlignment v_align = ui::VerticalAlignment::CENTER, ui::HorizontalAlignment h_align = ui::HorizontalAlignment::CENTER, bool draw_box_outline = true, bool draw_box_bkg = true, int box_padding_ud = 0, int box_padding_lr = 1, drawing::OutlineType outline_type = drawing::OutlineType::Line)
   {
-    if (message_empty && !messages.empty())
+    if (message_empty && has_messages())
     {
       trg_time = time;
-      std::tie(curr_message, curr_level) = messages.back();
-      messages.pop_back();
+      std::tie(curr_message, curr_level, curr_duration_s) = fetch_next_message();
       str_len = static_cast<int>(curr_message.size());
       message_empty = false;
     }
     
-    if (time - trg_time <= duration && !curr_message.empty())
+    if (time - trg_time <= curr_duration_s && !curr_message.empty())
     {
       auto fg_color = get_fg_color();
       auto bg_color = get_bg_color();
@@ -80,6 +122,8 @@ public:
   }
   
   bool has_message(float time) const { return !message_empty; }
+  
+  void clear_curr_message() { curr_message.clear(); }
 };
 
 
