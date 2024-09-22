fae1f7eae7644b0b1b8ad389c015c37fa104c657
Rasmus Anthin <scratty@gmail.com>
2024-09-22 23:37:26 +0200

    Added unit_tests.cpp.

---
+++ Tests/unit_tests.cpp
@@ -0,0 +1,18 @@
+//
+//  unit_tests.cpp
+//  Core
+//
+//  Created by Rasmus Anthin on 2024-09-21.
+//
+
+#include "Rectangle_tests.h"
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+  std::cout << "### Rectangle Tests ###" << std::endl;
+  ttl::unit_tests();
+  
+  return 0;
+}
