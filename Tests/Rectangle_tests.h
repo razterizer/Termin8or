6bf9fe8d0aae1a63e06e2c3246288f88152d35bb
Rasmus Anthin <scratty@gmail.com>
2024-09-22 23:32:20 +0200

    Adding stub Rectangle_tests.h.

---
+++ Tests/Rectangle_tests.h
@@ -0,0 +1,12 @@
+//
+//  Rectangle_tests.h
+//  Termin8or
+//
+//  Created by Rasmus Anthin on 2024-09-22.
+//
+
+#ifndef Rectangle_tests_h
+#define Rectangle_tests_h
+
+
+#endif /* Rectangle_tests_h */
