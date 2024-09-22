6cd4d6746d0d559b31c8236c6da45e4dc82a5783
Rasmus Anthin <scratty@gmail.com>
2024-09-22 23:32:45 +0200

    Adding stub tests in Rectangle_tests.h.

--- Tests/Rectangle_tests.h
+++ Tests/Rectangle_tests.h
@@ -5,8 +5,74 @@
 //  Created by Rasmus Anthin on 2024-09-22.
 //
 
-#ifndef Rectangle_tests_h
-#define Rectangle_tests_h
+#pragma once
+#include "../Rectangle.h"
+#include <iostream>
+#include <cassert>
 
+namespace datetime
+{
 
-#endif /* Rectangle_tests_h */
+  void unit_tests()
+  {
+    RC pt0 { 0, 0 };
+    RC pt1 { 1, 0 };
+    RC pt2 { 0, 1 };
+    RC pt3 { -1, 0 };
+    RC pt4 { 0, -1 };
+  
+    {
+      Rectangle rec;
+      
+      assert(rec.r == 0);
+      assert(rec.c == 0);
+      assert(rec.r_len == 0);
+      assert(rec.c_len == 0);
+      
+      assert(!rec.is_inside(0, 0));
+      assert(!rec.is_inside(1, 0));
+      assert(!rec.is_inside(0, 1));
+      assert(!rec.is_inside(-1, 0));
+      assert(!rec.is_inside(0, -1));
+      assert(!rec.is_inside(pt0));
+      assert(!rec.is_inside(pt1));
+      assert(!rec.is_inside(pt2));
+      assert(!rec.is_inside(pt3));
+      assert(!rec.is_inside(pt4));
+      
+      assert(!rec.is_inside_offs(0, 0, -1));
+      assert(!rec.is_inside_offs(1, 0, -1));
+      assert(!rec.is_inside_offs(0, 1, -1));
+      assert(!rec.is_inside_offs(-1, 0, -1));
+      assert(!rec.is_inside_offs(0, -1, -1));
+      assert(!rec.is_inside_offs(pt0, -1));
+      assert(!rec.is_inside_offs(pt1, -1));
+      assert(!rec.is_inside_offs(pt2, -1));
+      assert(!rec.is_inside_offs(pt3, -1));
+      assert(!rec.is_inside_offs(pt4, -1));
+      
+      assert(rec.is_inside_offs(0, 0, +1));
+      assert(!rec.is_inside_offs(1, 0, +1));
+      assert(!rec.is_inside_offs(0, 1, +1));
+      assert(rec.is_inside_offs(-1, 0, +1));
+      assert(rec.is_inside_offs(0, -1, +1));
+      assert(rec.is_inside_offs(pt0, +1));
+      assert(!rec.is_inside_offs(pt1, +1));
+      assert(!rec.is_inside_offs(pt2, +1));
+      assert(rec.is_inside_offs(pt3, +1));
+      assert(rec.is_inside_offs(pt4, +1));
+      
+      assert(rec.is_inside_offs(0, 0, +2));
+      assert(rec.is_inside_offs(1, 0, +2));
+      assert(rec.is_inside_offs(0, 1, +2));
+      assert(rec.is_inside_offs(-1, 0, +2));
+      assert(rec.is_inside_offs(0, -1, +2));
+      assert(rec.is_inside_offs(pt0, +2));
+      assert(rec.is_inside_offs(pt1, +2));
+      assert(rec.is_inside_offs(pt2, +2));
+      assert(rec.is_inside_offs(pt3, +2));
+      assert(rec.is_inside_offs(pt4, +2));
+    }
+  }
+  
+}
