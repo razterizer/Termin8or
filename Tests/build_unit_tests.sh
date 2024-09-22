80a2104974a5c981eaac9e05557a90e8e2e00614
Rasmus Anthin <scratty@gmail.com>
2024-09-22 23:38:49 +0200

    Added build script for the unit tests (copied directly from the Core repo).

---
+++ Tests/build_unit_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+additional_flags="-I../.."
+
+../build.sh unit_tests "$1" "${additional_flags[@]}"
+
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
