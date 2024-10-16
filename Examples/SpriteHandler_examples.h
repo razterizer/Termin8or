5132c113587f003b3f4d1e5bef6e40142c69e43e
Rasmus Anthin <scratty@gmail.com>
2024-10-16 03:03:48 +0200

    SpriteHandler.h: * Added support for animated sprites. All sprites are animated, but for a static sprite, just add one animation frame. * Animation can be controlled using a frame-to-texture functor which makes the system quite flexible. * I suppose that there should be additional arguments to the functor in the future such as simulation-time and/or real-time. * Updated example with animations.

--- Examples/SpriteHandler_examples.h
+++ Examples/SpriteHandler_examples.h
@@ -25,39 +25,87 @@ namespace sprite_handler
     
     auto* sprite0 = sprh.create_sprite("spaceship");
     sprite0->init(4, 5);
-    sprite0->set_sprite_chars(
-                              ' ', ' ', '_', ' ', ' ',
-                              ' ', '/', '#', '\\', ' ',
-                              '[', '-', '-', '-', ']',
-                              ' ', '{', '#', '}', ' '
-                              );
-    sprite0->set_sprite_fg_colors(
-                                  Color::Default, Color::Default, Color::White, Color::Default, Color::Default,
-                                  Color::Default, Color::Red, Color::Magenta, Color::Red, Color::Default,
-                                  Color::Green, Color::Cyan, Color::Cyan, Color::Cyan, Color::Green,
-                                  Color::Default, Color::Yellow, Color::Magenta, Color::Yellow, Color::Default
-                                  );
-    sprite0->set_sprite_bg_colors(
-                                  Color::Transparent2, Color::Transparent2, Color::Transparent2, Color::Transparent2, Color::Transparent2,
-                                  Color::Transparent2, Color::Transparent2, Color::DarkMagenta, Color::Transparent2, Color::Transparent2,
-                                  Color::Transparent2, Color::DarkCyan, Color::DarkCyan, Color::DarkCyan, Color::Transparent2,
-                                  Color::Transparent2, Color::Transparent2, Color::DarkMagenta, Color::Transparent2, Color::Transparent2
-                                  );
+    sprite0->init_frame(0);
+    sprite0->set_sprite_chars(0,
+      ' ', ' ', '_', ' ', ' ',
+      ' ', '/', '#', '\\', ' ',
+      '[', '-', '-', '-', ']',
+      ' ', '{', '#', '}', ' '
+    );
+    sprite0->set_sprite_fg_colors(0,
+      Color::Default, Color::Default, Color::White, Color::Default, Color::Default,
+      Color::Default, Color::Red, Color::Magenta, Color::Red, Color::Default,
+      Color::Green, Color::Cyan, Color::Cyan, Color::Cyan, Color::Green,
+      Color::Default, Color::Yellow, Color::Magenta, Color::Yellow, Color::Default
+    );
+    sprite0->set_sprite_bg_colors(0,
+      Color::Transparent2, Color::Transparent2, Color::Transparent2, Color::Transparent2, Color::Transparent2,
+      Color::Transparent2, Color::Transparent2, Color::DarkMagenta, Color::Transparent2, Color::Transparent2,
+      Color::Transparent2, Color::DarkCyan, Color::DarkCyan, Color::DarkCyan, Color::Transparent2,
+      Color::Transparent2, Color::Transparent2, Color::DarkMagenta, Color::Transparent2, Color::Transparent2
+    );
+    sprite0->init_frame(1);
+    sprite0->set_sprite_chars(1,
+      ' ', ' ', '_', ' ', ' ',
+      ' ', '/', '#', '\\', ' ',
+      '[', '-', '-', '-', ']',
+      ' ', '{', '#', '}', ' '
+    );
+    sprite0->set_sprite_fg_colors(1,
+      Color::Default, Color::Default, Color::White, Color::Default, Color::Default,
+      Color::Default, Color::Red, Color::Magenta, Color::Red, Color::Default,
+      Color::Green, Color::Cyan, Color::Cyan, Color::Cyan, Color::Green,
+      Color::Default, Color::Yellow, Color::Yellow, Color::Yellow, Color::Default
+    );
+    sprite0->set_sprite_bg_colors(1,
+      Color::Transparent2, Color::Transparent2, Color::Transparent2, Color::Transparent2, Color::Transparent2,
+      Color::Transparent2, Color::Transparent2, Color::DarkMagenta, Color::Transparent2, Color::Transparent2,
+      Color::Transparent2, Color::DarkCyan, Color::DarkCyan, Color::DarkCyan, Color::Transparent2,
+      Color::Transparent2, Color::Transparent2, Color::DarkYellow, Color::Transparent2, Color::Transparent2
+    );
+    sprite0->func_frame_to_texture = [](int anim_frame)
+    {
+      auto anim = anim_frame % 14;
+      if (anim < 8)
+        return 0;
+      return anim % 2;
+    };
     
     auto* sprite1 = sprh.create_sprite("alien");
     sprite1->init(2, 3);
-    sprite1->set_sprite_chars(
-                              ' ', '^', ' ',
-                              '%', 'U', '%'
-                              );
-    sprite1->set_sprite_fg_colors(
-                                  Color::Default, Color::Green, Color::Default,
-                                  Color::Yellow, Color::DarkYellow, Color::Yellow
-                                  );
-    sprite1->set_sprite_bg_colors(
-                                  Color::Transparent2, Color::Transparent2, Color::Transparent2,
-                                  Color::Transparent2, Color::DarkYellow, Color::Transparent2
-                                  );
+    sprite1->init_frame(0);
+    sprite1->set_sprite_chars(0,
+      ' ', '^', ' ',
+      '%', 'U', '%'
+    );
+    sprite1->set_sprite_fg_colors(0,
+      Color::Default, Color::Green, Color::Default,
+      Color::Yellow, Color::DarkYellow, Color::Yellow
+    );
+    sprite1->set_sprite_bg_colors(0,
+      Color::Transparent2, Color::Transparent2, Color::Transparent2,
+      Color::Transparent2, Color::DarkYellow, Color::Transparent2
+    );
+    sprite1->init_frame(1);
+    sprite1->set_sprite_chars(1,
+      ' ', '^', ' ',
+      '#', 'U', '#'
+    );
+    sprite1->set_sprite_fg_colors(1,
+      Color::Default, Color::Green, Color::Default,
+      Color::DarkYellow, Color::DarkYellow, Color::DarkYellow
+    );
+    sprite1->set_sprite_bg_colors(1,
+      Color::Transparent2, Color::Transparent2, Color::Transparent2,
+      Color::Transparent2, Color::DarkYellow, Color::Transparent2
+    );
+    sprite1->func_frame_to_texture = [](int anim_frame)
+    {
+      auto anim = anim_frame % 40;
+      if (anim < 34)
+        return 0;
+      return anim % 2;
+    };
     
     for (int i = -3; i < 20; ++i)
     {
@@ -66,13 +114,14 @@ namespace sprite_handler
         sprite0->pos.r = i;
         sprite0->pos.c = i%2==0 ? j : 35-j;
         
-        auto t = static_cast<float>((i + 3)*44 + (j + 5))/(23.f*45.f);
+        int anim_frame = (i + 3)*44 + (j + 5);
+        auto t = static_cast<float>(anim_frame)/(23.f*45.f);
         sprite1->pos.c = math::roundI(20.f + 6.f*std::cos(math::c_2pi * 7 * t));
         sprite1->pos.r = math::roundI(10.f + 4*std::sin(math::c_2pi * 10 * t));
         
         return_cursor();
         sh.clear();
-        sprh.draw(sh);
+        sprh.draw(sh, anim_frame);
         sh.print_screen_buffer(tt, Color::Black);
         Delay::sleep(0'200'000);
       }
--- SpriteHandler.h
+++ SpriteHandler.h
@@ -19,7 +19,7 @@ class Sprite
   template<typename T, typename... Args>
   void set_sprite_data(std::vector<T>& target, Args... args)
   {
-    const size_t sprite_size = texture.area;
+    const size_t sprite_size = area;
     if (sizeof...(args) != sprite_size)
       throw std::invalid_argument("Number of arguments must match sprite size.");
     target = {args...}; // Unpack and assign to the target vector
@@ -27,49 +27,66 @@ class Sprite
 
 public:
   RC pos { 0, 0 };
+  RC size { 0, 0 };
+  int area = 0;
+  
   int layer_id = 0; // 0 is the bottom layer.
   bool enabled = true;
-  drawing::Texture texture;
+  
+  std::vector<drawing::Texture> texture_frames;
   
   // Initialize the sprite's dimensions (NR and NC)
   void init(int NR, int NC)
   {
+    size = { NR, NC };
+    area = NR * NC;
+  }
+  
+  void init_frame(int anim_frame)
+  {
+    auto& texture = stlutils::at_growing(texture_frames, anim_frame);
     texture.clear();
-    texture.area = NR * NC;
-    texture.size = { NR, NC };
-    texture.characters.resize(NR * NC);
-    texture.fg_colors.resize(NR * NC);
-    texture.bg_colors.resize(NR * NC);
-    texture.materials.resize(NR * NC);
+    texture.area = area;
+    texture.size = size;
+    texture.characters.resize(area);
+    texture.fg_colors.resize(area);
+    texture.bg_colors.resize(area);
+    texture.materials.resize(area);
   }
   
   // Set sprite characters
   template<typename... Chars>
-  void set_sprite_chars(Chars... ch)
+  void set_sprite_chars(int anim_frame, Chars... ch)
   {
+    auto& texture = stlutils::at_growing(texture_frames, anim_frame);
     set_sprite_data(texture.characters, ch...);
   }
   
   // Set sprite foreground colors
   template<typename... Colors>
-  void set_sprite_fg_colors(Colors... fg_color)
+  void set_sprite_fg_colors(int anim_frame, Colors... fg_color)
   {
+    auto& texture = stlutils::at_growing(texture_frames, anim_frame);
     set_sprite_data(texture.fg_colors, fg_color...);
   }
   
   // Set sprite background colors
   template<typename... Colors>
-  void set_sprite_bg_colors(Colors... bg_color)
+  void set_sprite_bg_colors(int anim_frame, Colors... bg_color)
   {
+    auto& texture = stlutils::at_growing(texture_frames, anim_frame);
     set_sprite_data(texture.bg_colors, bg_color...);
   }
   
   // Set sprite materials
   template<typename... Materials>
-  void set_sprite_materials(Materials... mat)
+  void set_sprite_materials(int anim_frame, Materials... mat)
   {
+    auto& texture = stlutils::at_growing(texture_frames, anim_frame);
     set_sprite_data(texture.materials, mat...);
   }
+  
+  std::function<int(int)> func_frame_to_texture = [](int anim_frame) -> int { return 0; };
 };
 
 // /////////////////////////////////////
@@ -98,7 +115,7 @@ public:
   }
   
   template<int NR, int NC>
-  void draw(ScreenHandler<NR, NC>& sh) const
+  void draw(ScreenHandler<NR, NC>& sh, int anim_frame) const
   {
     int max_layer_id = 0;
     for (const auto& sprite_pair : m_sprites)
@@ -111,11 +128,16 @@ public:
       {
         const auto& sprite = sprite_pair.second;
         if (sprite->enabled && sprite->layer_id == layer_id)
+        {
+          int tex_id = sprite->func_frame_to_texture(anim_frame);
+          auto& texture = stlutils::at_growing(sprite->texture_frames, tex_id);
+        
           drawing::draw_box_textured(sh,
                           sprite->pos.r - 1, sprite->pos.c - 1,
-                          sprite->texture.size.r + 2, sprite->texture.size.c + 2,
+                          texture.size.r + 2, texture.size.c + 2,
                           drawing::SolarDirection::Zenith,
-                          sprite->texture);
+                          texture);
+        }
       }
     }
   }
