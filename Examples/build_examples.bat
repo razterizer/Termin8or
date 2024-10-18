5c3d5b86969893cbeeb7af10626d0594b3ffdc56
Rasmus Anthin <scratty@gmail.com>
2024-10-18 17:39:01 +0200

    Adding VC++ build script for examples.

---
+++ Examples/build_examples.bat
@@ -0,0 +1,11 @@
+call ..\..\Core\build.bat
+
+mkdir x86\Release
+
+cl examples.cpp /I..\.. /std:c++20 /Fex86\Release\examples.exe
+
+if %errorlevel% neq 0 (
+    echo Build failed with error code %errorlevel%.
+    exit /b %errorlevel%
+)
+echo Build succeeded.
