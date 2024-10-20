71f9bcf83e76825b440c814e61cef18cf591f88c
Rasmus Anthin <scratty@gmail.com>
2024-10-21 00:21:20 +0200

    Added header file CollisionHandler.h.

---
+++ CollisionHandler.h
@@ -0,0 +1,12 @@
+//
+//  CollisionHandler.h
+//  Termin8or
+//
+//  Created by Rasmus Anthin on 2024-10-21.
+//
+
+#ifndef CollisionHandler_h
+#define CollisionHandler_h
+
+
+#endif /* CollisionHandler_h */
