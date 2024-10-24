8dcd09da3e7f2e91f47d7567454ff16a3994d076
Rasmus Anthin <scratty@gmail.com>
2024-10-25 01:19:59 +0200

    Vec2.h: * Adding some constructors and operators.

--- Vec2.h
+++ Vec2.h
@@ -14,6 +14,13 @@ struct Vec2
   float r = 0.f;
   float c = 0.f;
   
+  Vec2() = default;
+  
+  Vec2(float rr, float cc)
+    : r(rr)
+    , c(cc)
+  {}
+  
   Vec2(const Vec2& v)
   {
     r = v.r;
@@ -27,4 +34,49 @@ struct Vec2
   }
   
   operator RC() { return { math::roundI(r), math::roundI(c) }; }
+  
+  const Vec2& operator+() const
+  {
+    return *this;
+  }
+  
+  Vec2 operator-() const
+  {
+    return { -r, -c };
+  }
+  
+  Vec2 operator+(const Vec2& v) const
+  {
+    return { r + v.r, c + v.c };
+  }
+  Vec2 operator-(const Vec2& v) const
+  {
+    return { r - v.r, c - v.c };
+  }
+  Vec2& operator+=(const Vec2& v)
+  {
+    this->r += v.r;
+    this->c += v.c;
+    return *this;
+  }
+  Vec2& operator-=(const Vec2& v)
+  {
+    this->r -= v.r;
+    this->c -= v.c;
+    return *this;
+  }
+  Vec2 operator*(float v) const
+  {
+    return { r * v, c * v };
+  }
+  Vec2 operator/(float v) const
+  {
+    return { r / v, c / v };
+  }
+  Vec2& operator/=(float v)
+  {
+    this->r /= v;
+    this->c /= v;
+    return *this;
+  }
 };
