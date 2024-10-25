42c3f96fd56f698bbdd64ce4f74199f995d4c74d
Rasmus Anthin <scratty@gmail.com>
2024-10-25 19:57:44 +0200

    DynamicsSystem.h: * Adding some rigid body stuff.

--- Dynamics/DynamicsSystem.h
+++ Dynamics/DynamicsSystem.h
@@ -6,6 +6,10 @@
 //
 
 #pragma once
+#include "RigidBody.h"
+#include "../SpriteHandler.h"
+#include <vector>
+#include <memory>
 
 
 namespace dynamics
@@ -13,8 +17,21 @@ namespace dynamics
 
   class DynamicsSystem
   {
+    std::vector<std::unique_ptr<RigidBody>> m_rigid_bodies;
   
+  public:
   
+    RigidBody* add_rigid_body(Sprite* sprite)
+    {
+      auto& rb = m_rigid_bodies.emplace_back(std::make_unique<RigidBody>());
+      rb->init_from_sprite(sprite);
+      return rb.get();
+    }
+    
+    void update()
+    {
+      
+    }
   };
 
 }
