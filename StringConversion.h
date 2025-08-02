5d57490e937a82f092da12c91a2879cc0087e6bf
Rasmus Anthin <scratty@gmail.com>
2025-08-03 01:35:42 +0200

    StringConversion.h: * Changing namespace from ttl to str. * Added functions for converting to/from strings with an RC object.

--- StringConversion.h
+++ StringConversion.h
@@ -8,28 +8,45 @@
 #pragma once
 
 #include "Rectangle.h"
+#include "RC.h"
 #include <Core/StringHelper.h>
 #include <optional>
 
-namespace ttl
+namespace str
 {
   
-  std::string rectangle_to_str(const Rectangle& rec)
+  std::string rectangle_to_str(const ttl::Rectangle& rec)
   {
     return "{ { " + std::to_string(rec.r) + ", " + std::to_string(rec.c) + " }, { " + std::to_string(rec.r_len) + ", " + std::to_string(rec.c_len) + " } }";
   }
   
-  std::optional<Rectangle> str_to_rectangle(const std::string& str)
+  std::optional<ttl::Rectangle> str_to_rectangle(const std::string& str)
   {
     auto tokens = str::tokenize(str, { ',', ' ' }, { '{', '}' });
     if (tokens.size() != 4)
       return std::nullopt;
-    Rectangle rec;
+    ttl::Rectangle rec;
     rec.r = std::atoi(tokens[0].c_str());
     rec.c = std::atoi(tokens[1].c_str());
     rec.r_len = std::atoi(tokens[2].c_str());
     rec.c_len = std::atoi(tokens[3].c_str());
     return rec;
+  }
+  
+  std::string rc_to_str(const RC& rc)
+  {
+    return rc.str();
+  }
+  
+  std::optional<RC> str_to_rc(const std::string& str)
+  {
+    auto tokens = str::tokenize(str, { ',', ' ', '(', ')' });
+    if (tokens.size() != 2)
+      return std::nullopt;
+    RC rc;
+    rc.r = std::atoi(tokens[0].c_str());
+    rc.c = std::atoi(tokens[1].c_str());
+    return rc;
   }
 
 }
