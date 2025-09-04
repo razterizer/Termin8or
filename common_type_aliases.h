1528b8a4edc582b9149a6c1f4c8033ad08c59601
Rasmus Anthin <scratty@gmail.com>
2025-09-04 19:43:39 +0200

    Added namespace_aliases.h and common_type_aliases.h for your convenience.

---
+++ common_type_aliases.h
@@ -0,0 +1,13 @@
+//
+//  common_type_aliases.h
+//  Termin8or
+//
+//  Created by Rasmus Anthin on 2025-09-04.
+//
+
+#pragma once
+
+using Color = t8::Color;
+using Style = t8::color::Style;
+using RC = t8::RC;
+using Rectangle = t8::Rectangle;
---
+++ namespace_aliases.h
@@ -0,0 +1,23 @@
+//
+//  namespace_aliases.h
+//  Termin8or
+//
+//  Created by Rasmus Anthin on 2025-09-04.
+//
+
+#pragma once
+
+// Core aliases
+namespace t8c  = t8::color;
+namespace t8s  = t8::screen;
+namespace t8i  = t8::input;
+namespace t8d  = t8::drawing;
+namespace t8l  = t8::log;
+
+// Extras aliases
+namespace t8xs = t8x::screen;
+namespace t8xd = t8x::drawing;
+namespace t8xp = t8x::physics;
+namespace t8xsp = t8x::sprite;
+namespace t8xu = t8x::ui;
+namespace t8xf = t8x::fonts;
