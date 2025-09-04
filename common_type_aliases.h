b5670a0e3f6f561922cfc6f1e045227c00c767e1
Rasmus Anthin <scratty@gmail.com>
2025-09-04 20:07:07 +0200

    common_type_aliases.h: * Added some more common type aliases.

--- common_type_aliases.h
+++ common_type_aliases.h
@@ -7,7 +7,19 @@
 
 #pragma once
 
+// core types
 using Color = t8::Color;
 using Style = t8::color::Style;
 using RC = t8::RC;
 using Rectangle = t8::Rectangle;
+
+// screen
+template<int NR, int NC>
+using ScreenHandler = t8::screen::ScreenHandler<NR, NC>;
+
+// drawing
+using Textel = t8::drawing::Textel;
+using Texture = t8::drawing::Texture;
+
+// input
+using SpecialKey = t8::input::SpecialKey;
