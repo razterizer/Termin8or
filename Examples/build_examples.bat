dc0eb43228750a781e28139bffb83362084760ae
Rasmus Anthin <scratty@gmail.com>
2025-11-10 01:34:01 +0100

    Hmm. Let's see if upgrading the build_examples.bat script resolves the windows release workflow! Hopefully it does.

--- Examples/build_examples.bat
+++ Examples/build_examples.bat
@@ -1,8 +1,27 @@
+REM call ..\..\Core\build.bat
+REM
+REM mkdir x86\Release
+REM
+REM cl examples.cpp /I..\.. /I..\include\Termin8or /std:c++20 /Fex86\Release\examples.exe /Fox86\Release\examples.obj
+REM
+REM if %errorlevel% neq 0 (
+REM     echo Build failed with error code %errorlevel%.
+REM     exit /b %errorlevel%
+REM )
+REM
+REM echo Build succeeded.
+
 call ..\..\Core\build.bat
 
-mkdir x86\Release
+cd examples.vs
 
-cl examples.cpp /I..\.. /I..\include\Termin8or /std:c++20 /Fex86\Release\examples.exe /Fox86\Release\examples.obj
+SET configuration="Release"
+IF "%~1" == "Debug" SET configuration="Debug"
+SET target="x64"
+IF "%~2" == "x86" SET target="x86"
+msbuild examples.vs.sln /p:Configuration=%configuration% /p:Platform=%target%
+
+cd ..
 
 if %errorlevel% neq 0 (
     echo Build failed with error code %errorlevel%.
