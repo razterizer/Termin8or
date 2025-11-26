6de8036d06b0c69f33a7af6f2924cc124ad3a42e
Rasmus Anthin <scratty@gmail.com>
2025-11-26 06:42:47 +0100

    ScreenScaling.h: map color2rgba now uses Color instead of Color16 as a key and maps all 256  colors (+ Transparent2).

--- include/Termin8or/screen/ScreenScaling.h
+++ include/Termin8or/screen/ScreenScaling.h
@@ -30,26 +30,53 @@ namespace t8x
   
 // ////////////////////////////////////////////////
 
-  std::map<Color16, RGBA> color2rgba
+  static std::map<Color, RGBA> color2rgba = []()
   {
-    { Color16::White, RGBA { 1, 1, 1 } },
-    { Color16::Cyan, RGBA { 0, 1, 1 } },
-    { Color16::Magenta, RGBA { 1, 0, 1 } },
-    { Color16::Blue, RGBA { 0, 0, 1 } },
-    { Color16::Yellow, RGBA { 1, 1, 0 } },
-    { Color16::Green, RGBA { 0, 1, 0 } },
-    { Color16::Red, RGBA { 1, 0, 0 } },
-    { Color16::LightGray, RGBA { 0.8, 0.8, 0.8 } },
-    { Color16::DarkGray, RGBA { 0.4, 0.4, 0.4 } },
-    { Color16::DarkCyan, RGBA { 0, 0.4, 0.4 } },
-    { Color16::DarkMagenta, RGBA { 0.4, 0, 0.4 } },
-    { Color16::DarkBlue, RGBA { 0, 0, 0.4 } },
-    { Color16::DarkYellow, RGBA { 0.4, 0.4, 0 } },
-    { Color16::DarkGreen, RGBA { 0, 0.4, 0 } },
-    { Color16::DarkRed, RGBA { 0.4, 0, 0 } },
-    { Color16::Black, RGBA { 0, 0, 0 } },
-    { Color16::Transparent2, RGBA { 0, 0, 0, 0 } },
-  };
+    auto colmap = std::map<Color, RGBA>
+    {
+      { Color16::White, RGBA { 1, 1, 1 } },
+      { Color16::Cyan, RGBA { 0, 1, 1 } },
+      { Color16::Magenta, RGBA { 1, 0, 1 } },
+      { Color16::Blue, RGBA { 0, 0, 1 } },
+      { Color16::Yellow, RGBA { 1, 1, 0 } },
+      { Color16::Green, RGBA { 0, 1, 0 } },
+      { Color16::Red, RGBA { 1, 0, 0 } },
+      { Color16::LightGray, RGBA { 0.8, 0.8, 0.8 } },
+      { Color16::DarkGray, RGBA { 0.4, 0.4, 0.4 } },
+      { Color16::DarkCyan, RGBA { 0, 0.4, 0.4 } },
+      { Color16::DarkMagenta, RGBA { 0.4, 0, 0.4 } },
+      { Color16::DarkBlue, RGBA { 0, 0, 0.4 } },
+      { Color16::DarkYellow, RGBA { 0.4, 0.4, 0 } },
+      { Color16::DarkGreen, RGBA { 0, 0.4, 0 } },
+      { Color16::DarkRed, RGBA { 0.4, 0, 0 } },
+      { Color16::Black, RGBA { 0, 0, 0 } },
+      { Color16::Transparent2, RGBA { 0, 0, 0, 0 } },
+    };
+    
+    const int steps[6] = {0, 95, 135, 175, 215, 255};
+    
+    for (int r = 0; r < 6; r++)
+      for (int g = 0; g < 6; g++)
+        for (int b = 0; b < 6; b++)
+        {
+          int idx = 16 + 36*r + 6*g + b;
+          colmap[Color(idx)] = RGBA
+          {
+            steps[r] / 255.0,
+            steps[g] / 255.0,
+            steps[b] / 255.0
+          };
+        }
+        
+    for (int i = 232; i <= 255; i++)
+    {
+      int v = 8 + (i - 232) * 10;     // 8, 18, 28, ..., 238
+      double f = v / 255.0;
+      colmap[Color(i)] = RGBA {f, f, f};
+    }
+    
+    return colmap;
+  }();
 
   char find_closest_val(double shading_value) const
   {
