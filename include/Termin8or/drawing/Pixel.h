019922beadcbe0c3a464cbd3d3b2a5265d551efb
Rasmus Anthin <scratty@gmail.com>
2025-11-28 09:05:18 +0100

    Pixel.h: Pixel struct now supports 256 colors (actually 259 colors to be exact).

--- include/Termin8or/drawing/Pixel.h
+++ include/Termin8or/drawing/Pixel.h
@@ -6,17 +6,17 @@ namespace t8x
   
   struct Pixel
   {
-    using Color16 = t8::Color16;
+    using Color = t8::Color;
   
-    Pixel(char c, Color16 fg, Color16 bg,
+    Pixel(char c, Color fg, Color bg,
           int rr = -1, int cc = -1, int ln_idx = -1, int ch_idx = -1, bool enabled = true)
       : ch(c), str(1, ch), fg_color(fg), bg_color(bg),
         r(rr), c(cc), r_orig(rr), c_orig(cc),
         line_idx(ln_idx), char_idx(ch_idx), active(enabled) { }
     char ch;
     std::string str;
-    Color16 fg_color;
-    Color16 bg_color;
+    Color fg_color;
+    Color bg_color;
     int r = -1;
     int c = -1;
     int r_orig = -1;
