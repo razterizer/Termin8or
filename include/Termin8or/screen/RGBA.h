6c840793a64d9f7385589ebaf4bceb39f8d49283
Rasmus Anthin <scratty@gmail.com>
2025-11-30 22:47:40 +0100

    Full migration of RGBA struct to RGBA.h from ScreenScaling.h and moving the color2rgba map from ScreenScaling.h to Color.h.

--- include/Termin8or/screen/RGBA.h
+++ include/Termin8or/screen/RGBA.h
@@ -23,62 +23,9 @@ namespace t8
   };
   inline RGBA operator*(double v, const RGBA& rgba) { return { v*rgba.r, v*rgba.g, v*rgba.b, v*rgba.a }; }
   
-  double clamp(double t) { return math::clamp(t, 0., 1.); }
-  RGBA clamp(const RGBA& t) { return RGBA { math::clamp(t.r, 0., 1.), math::clamp(t.g, 0., 1.), math::clamp(t.b, 0., 1.), math::clamp(t.a, 0., 1.) }; }
-  
   inline double luminance(const RGBA& c)
   {
     return 0.2126 * c.r + 0.7152 * c.g + 0.0722 * c.b;
   }
-  
-  // ////////////////////////////////////////////////
-
-  static std::map<Color, RGBA> color2rgba = []()
-  {
-    auto colmap = std::map<Color, RGBA>
-    {
-      { Color16::White, RGBA { 1, 1, 1 } },
-      { Color16::Cyan, RGBA { 0, 1, 1 } },
-      { Color16::Magenta, RGBA { 1, 0, 1 } },
-      { Color16::Blue, RGBA { 0, 0, 1 } },
-      { Color16::Yellow, RGBA { 1, 1, 0 } },
-      { Color16::Green, RGBA { 0, 1, 0 } },
-      { Color16::Red, RGBA { 1, 0, 0 } },
-      { Color16::LightGray, RGBA { 0.8, 0.8, 0.8 } },
-      { Color16::DarkGray, RGBA { 0.4, 0.4, 0.4 } },
-      { Color16::DarkCyan, RGBA { 0, 0.4, 0.4 } },
-      { Color16::DarkMagenta, RGBA { 0.4, 0, 0.4 } },
-      { Color16::DarkBlue, RGBA { 0, 0, 0.4 } },
-      { Color16::DarkYellow, RGBA { 0.4, 0.4, 0 } },
-      { Color16::DarkGreen, RGBA { 0, 0.4, 0 } },
-      { Color16::DarkRed, RGBA { 0.4, 0, 0 } },
-      { Color16::Black, RGBA { 0, 0, 0 } },
-      { Color16::Transparent2, RGBA { 0, 0, 0, 0 } },
-    };
-    
-    const int steps[6] = {0, 95, 135, 175, 215, 255};
-    
-    for (int r = 0; r < 6; r++)
-      for (int g = 0; g < 6; g++)
-        for (int b = 0; b < 6; b++)
-        {
-          int idx = 16 + 36*r + 6*g + b;
-          colmap[Color(idx)] = RGBA
-          {
-            steps[r] / 255.0,
-            steps[g] / 255.0,
-            steps[b] / 255.0
-          };
-        }
-        
-    for (int i = 232; i <= 255; i++)
-    {
-      int v = 8 + (i - 232) * 10;     // 8, 18, 28, ..., 238
-      double f = v / 255.0;
-      colmap[Color(i)] = RGBA {f, f, f};
-    }
-    
-    return colmap;
-  }();
 
 }
--- include/Termin8or/screen/ScreenScaling.h
+++ include/Termin8or/screen/ScreenScaling.h
@@ -12,71 +12,17 @@
 namespace t8x
 {
 
-  struct RGBA
-  {
-    RGBA() : r(0), g(0), b(0), a(1) {}
-    RGBA(double v) : r(v), g(v), b(v), a(v) {}
-    RGBA(double rv, double gv, double bv) : r(rv), g(gv), b(bv), a(1) {}
-    RGBA(double rv, double gv, double bv, double av) : r(rv), g(gv), b(bv), a(av) {}
-    double r = 0, g = 0, b = 0, a = 0;
-    RGBA operator+(const RGBA& other) { return { r+other.r, g+other.g, b+other.b, a+other.a }; }
-    RGBA operator*(const RGBA& other) { return { r*other.r, g*other.g, b*other.b, a*other.a }; }
-    RGBA operator*(double v) { return { r*v, g*v, b*v, a*v }; };
-  };
-  inline RGBA operator*(double v, const RGBA& rgba) { return { v*rgba.r, v*rgba.g, v*rgba.b, v*rgba.a }; }
-  
   double clamp(double t) { return math::clamp(t, 0., 1.); }
-  RGBA clamp(const RGBA& t) { return RGBA { math::clamp(t.r, 0., 1.), math::clamp(t.g, 0., 1.), math::clamp(t.b, 0., 1.), math::clamp(t.a, 0., 1.) }; }
-  
-// ////////////////////////////////////////////////
-
-  static std::map<Color, RGBA> color2rgba = []()
+  t8::RGBA clamp(const t8::RGBA& t)
   {
-    auto colmap = std::map<Color, RGBA>
+    return t8::RGBA
     {
-      { Color16::White, RGBA { 1, 1, 1 } },
-      { Color16::Cyan, RGBA { 0, 1, 1 } },
-      { Color16::Magenta, RGBA { 1, 0, 1 } },
-      { Color16::Blue, RGBA { 0, 0, 1 } },
-      { Color16::Yellow, RGBA { 1, 1, 0 } },
-      { Color16::Green, RGBA { 0, 1, 0 } },
-      { Color16::Red, RGBA { 1, 0, 0 } },
-      { Color16::LightGray, RGBA { 0.8, 0.8, 0.8 } },
-      { Color16::DarkGray, RGBA { 0.4, 0.4, 0.4 } },
-      { Color16::DarkCyan, RGBA { 0, 0.4, 0.4 } },
-      { Color16::DarkMagenta, RGBA { 0.4, 0, 0.4 } },
-      { Color16::DarkBlue, RGBA { 0, 0, 0.4 } },
-      { Color16::DarkYellow, RGBA { 0.4, 0.4, 0 } },
-      { Color16::DarkGreen, RGBA { 0, 0.4, 0 } },
-      { Color16::DarkRed, RGBA { 0.4, 0, 0 } },
-      { Color16::Black, RGBA { 0, 0, 0 } },
-      { Color16::Transparent2, RGBA { 0, 0, 0, 0 } },
+      math::clamp(t.r, 0., 1.),
+      math::clamp(t.g, 0., 1.),
+      math::clamp(t.b, 0., 1.),
+      math::clamp(t.a, 0., 1.)
     };
-    
-    const int steps[6] = {0, 95, 135, 175, 215, 255};
-    
-    for (int r = 0; r < 6; r++)
-      for (int g = 0; g < 6; g++)
-        for (int b = 0; b < 6; b++)
-        {
-          int idx = 16 + 36*r + 6*g + b;
-          colmap[Color(idx)] = RGBA
-          {
-            steps[r] / 255.0,
-            steps[g] / 255.0,
-            steps[b] / 255.0
-          };
-        }
-        
-    for (int i = 232; i <= 255; i++)
-    {
-      int v = 8 + (i - 232) * 10;     // 8, 18, 28, ..., 238
-      double f = v / 255.0;
-      colmap[Color(i)] = RGBA {f, f, f};
-    }
-    
-    return colmap;
-  }();
+  }
 
   char find_closest_val(double shading_value)
   {
@@ -87,14 +33,14 @@ namespace t8x
     return shading_charset[index];
   }
 
-  Color find_closest_val(RGBA shading_value)
+  Color find_closest_val(t8::RGBA shading_value)
   {
     // Character set representing different shades
     auto t = shading_value;
-    auto distance_squared = [&](Color color, const RGBA& rgba1)
+    auto distance_squared = [&](Color color, const t8::RGBA& rgba1)
     {
-      auto it = color2rgba.find(color);
-      if (it != color2rgba.end())
+      auto it = t8::color2rgba.find(color);
+      if (it != t8::color2rgba.end())
       {
         auto rgba0 = it->second;
         return math::distance_squared<double>(rgba0.r, rgba0.g, rgba0.b, rgba0.a,
@@ -139,13 +85,13 @@ namespace t8x
     }
   }
 
-  RGBA find_closest_shading_value(Color color)
+  t8::RGBA find_closest_shading_value(Color color)
   {
     auto col_idx = color.get_index();
     if (math::in_range(col_idx, t8::c_min_color_idx, t8::c_max_color_idx, Range::Closed))
     {
-      auto it = color2rgba.find(color);
-      if (it != color2rgba.end())
+      auto it = t8::color2rgba.find(color);
+      if (it != t8::color2rgba.end())
         return it->second;
     }
     return { 0, 0, 0, 0 }; // Transparent2
@@ -229,8 +175,8 @@ namespace t8x
                                        t8::ScreenHandler<NRo, NCo>& sh_dst)
   {
     auto new_screen_buffer = resample_data<char, double, NRi, NCi, NRo, NCo>(sh_src.screen_buffer, 0);
-    auto new_fg_color_buffer = resample_data<Color, RGBA, NRi, NCi, NRo, NCo>(sh_src.fg_color_buffer, 0);
-    auto new_bg_color_buffer = resample_data<Color, RGBA, NRi, NCi, NRo, NCo>(sh_src.bg_color_buffer, 0);
+    auto new_fg_color_buffer = resample_data<Color, t8::RGBA, NRi, NCi, NRo, NCo>(sh_src.fg_color_buffer, 0);
+    auto new_bg_color_buffer = resample_data<Color, t8::RGBA, NRi, NCi, NRo, NCo>(sh_src.bg_color_buffer, 0);
     sh_dst.overwrite_data(new_screen_buffer, new_fg_color_buffer, new_bg_color_buffer);
   }
   
