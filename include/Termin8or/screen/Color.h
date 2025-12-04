f239cfe713ed5d50181301b19ae2dc15eb327592
Rasmus Anthin <scratty@gmail.com>
2025-12-04 22:50:59 +0100

    Color.h: parse(): Simplifying parsing of nonconstant gray24 tokens (slight optimization too).

--- include/Termin8or/screen/Color.h
+++ include/Termin8or/screen/Color.h
@@ -629,7 +629,6 @@ namespace t8
       static const char* gray24_prefix = "gray24:";
       if (!remaining.empty() && (remaining[0] == '{' || remaining.starts_with(gray24_prefix)))
       {
-        bool compact = !remaining.starts_with(gray24_prefix);
         if (auto i = remaining.find('}'); i == std::string::npos)
         {
           std::cerr << "ERROR in Color::parse() : Unable to parse ']' token after rgb6 triplet!\n";
@@ -637,7 +636,7 @@ namespace t8
         }
         else
         {
-          if (!compact)
+          if (remaining.starts_with(gray24_prefix))
             start_idx += std::strlen(gray24_prefix);
           int end_idx = start_idx + static_cast<int>(i);
           auto substr = str.substr(start_idx, end_idx - start_idx + 1);
