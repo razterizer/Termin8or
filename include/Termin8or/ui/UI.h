99603efa03dc77a51f99b90b77552336067d148b
Rasmus Anthin <scratty@gmail.com>
2025-12-04 22:59:47 +0100

    UI.h: Upgraded ColorPicker so that it also supports rgb6 and gray24 color spaces. * You can pick and choose which ranges of the full 8-bit color range that you want to use in the color picker. * It seems robust enough for a commit now. * Broke out certain arguments into a new ColorPickerParams struct so that I can more easily add more parameters (instead of a lot of function arguments). * Added enum for cursor blinking mode; ColorPickerCursorBlinking. You can set this behaviour in the ColorPickerParams. * ColorPickerParams have a parameter: special_color_fg_hilite_color. It does what it says on the tin. * Added four enable_*_colors bool params in ColorPickerParams so you can choose which color ranges/spaces of the full 8-bit color range/space you want to use in your color picker instance. * Other related cool sub-features.

--- include/Termin8or/ui/UI.h
+++ include/Termin8or/ui/UI.h
@@ -60,6 +60,18 @@ namespace t8x
   enum class ButtonFrame { None, Space, SquareBrackets, AngleBrackets, Braces, Parentheses, Pipes };
   enum class TextFieldMode { Numeric, AlphaNumeric, Alphabetic, All };
   enum class ColorPickerCursorColoring { BlackWhite, Contrast };
+  enum class ColorPickerCursorBlinking { Persist, EveryOther };
+  
+  struct ColorPickerParams
+  {
+    ColorPickerCursorColoring fg_cursor_coloring = ColorPickerCursorColoring::BlackWhite;
+    ColorPickerCursorBlinking cursor_blinking = ColorPickerCursorBlinking::Persist;
+    Color special_color_fg_hilite_color = Color16::Cyan;
+    bool enable_special_colors = false;
+    bool enable_4bit_colors = false;
+    bool enable_rgb6_colors = false;
+    bool enable_gray24_colors = false;
+  };
   
   class Widget
   {
@@ -359,14 +371,33 @@ namespace t8x
   {
     Color fg_color_sel;
     Color fg_color_sel_hilite;
-    ColorPickerCursorColoring fg_cursor_coloring;
+    ColorPickerParams params;
     char select_char;
     char unselect_char = ' ';
-    std::string field;
-    int caret = 0;
-    static constexpr int c_num_colors = 16; // 16
-    // #FIXME: Add transparency (T2) support.
+    RC caret = { 0, 0 };
     bool wrapping = false;
+    static const int c_special_start = -3;
+    static const int c_special_end = -1;
+    static const int c_4bit_start = 0;
+    static const int c_4bit_end = 15;
+    static const int c_rgb6_start = 16;
+    static const int c_rgb6_end = 231;
+    static const int c_gray24_start = 232;
+    static const int c_gray24_end = 255;
+    int col_x_start = 0;
+    int col_x_end = 0;
+    int col_4b_start = 0;
+    int col_4b_end = 0;
+    int col_rgb6_start = 0;
+    int col_rgb6_end = 0;
+    int col_g24_start = 0;
+    int col_g24_end = 0;
+    int num_16color_colors = 0;
+    int num_16color_rows = 0;
+    int num_rgb6_rows = 0;
+    int num_gray24_rows = 0;
+    int num_rows = 0;
+    int r_max = 0;
     
     Color get_color(int idx) const
     {
@@ -374,16 +405,41 @@ namespace t8x
     }
     
   public:
-    ColorPicker(Color fg_sel, Color fg_sel_hilite, ColorPickerCursorColoring fg_cursor_coloring_scheme, int tab = 0, bool cursor_wrapping = false, char sel_char = '*', char unsel_char = ' ', bool sel = false)
+    ColorPicker(Color fg_sel, Color fg_sel_hilite,
+                const ColorPickerParams& cp_params,
+                int tab = 0, bool cursor_wrapping = false,
+                char sel_char = '*', char unsel_char = ' ',
+                bool sel = false)
       : Widget(tab, sel)
       , fg_color_sel(fg_sel)
       , fg_color_sel_hilite(fg_sel_hilite)
-      , fg_cursor_coloring(fg_cursor_coloring_scheme)
+      , params(cp_params)
       , select_char(sel_char)
       , unselect_char(unsel_char)
       , wrapping(cursor_wrapping)
+      , col_x_start(params.enable_special_colors ? c_special_start : 0)
+      , col_x_end(params.enable_special_colors ? c_special_end : -1)
+      , col_4b_start(params.enable_4bit_colors ? c_4bit_start : 0)
+      , col_4b_end(params.enable_4bit_colors ? c_4bit_end : -1)
+      , col_rgb6_start(params.enable_rgb6_colors ? c_rgb6_start : 0)
+      , col_rgb6_end(params.enable_rgb6_colors ? c_rgb6_end : -1)
+      , col_g24_start(params.enable_gray24_colors ? c_gray24_start : 0)
+      , col_g24_end(params.enable_gray24_colors ? c_gray24_end : -1)
     {
-      field = str::rep_char(unselect_char, c_num_colors);
+      if (params.enable_special_colors)
+        num_16color_colors += 3;
+      if (params.enable_4bit_colors)
+        num_16color_colors += 16;
+        
+      if (params.enable_special_colors || params.enable_4bit_colors)
+        num_16color_rows = 1;
+      if (params.enable_rgb6_colors)
+        num_rgb6_rows = 6;
+      if (params.enable_gray24_colors)
+        num_gray24_rows = 1;
+        
+      num_rows = std::max(1, num_16color_rows + num_rgb6_rows + num_gray24_rows);
+      r_max = num_rows - 1;
     }
     
     void update(SpecialKey curr_special_key)
@@ -391,59 +447,206 @@ namespace t8x
       if (!is_selected())
         return;
       
-      if (curr_special_key == SpecialKey::Left)
+      bool moved = false;
+      if (curr_special_key == SpecialKey::Up)
       {
-        caret--;
-        if (caret < 0)
-          caret = wrapping ? (c_num_colors - 1) : 0;
+        caret.r--;
+        if (caret.r < 0)
+          caret.r = wrapping ? r_max : 0;
+        moved = true;
+      }
+      else if (curr_special_key == SpecialKey::Down)
+      {
+        caret.r++;
+        if (caret.r > r_max)
+          caret.r = wrapping ? 0 : r_max;
+        moved = true;
+      }
+      else if (curr_special_key == SpecialKey::Left)
+      {
+        caret.c--;
+        moved = true;
       }
       else if (curr_special_key == SpecialKey::Right)
       {
-        caret++;
-        if (caret >= c_num_colors)
-          caret = wrapping ? 0 : c_num_colors - 1;
+        caret.c++;
+        moved = true;
+      }
+      
+      if (moved)
+      {
+        if (num_16color_colors > 0 && caret.r == 0)
+        {
+          int c_max = num_16color_colors - 1;
+          if (caret.c < 0)
+            caret.c = wrapping ? c_max : 0;
+          else if (caret.c > c_max)
+            caret.c = wrapping ? 0 : c_max;
+        }
+        else if (params.enable_rgb6_colors && math::in_r_co(caret.r, num_16color_rows, num_16color_rows + num_rgb6_rows))
+        {
+          if (caret.c < 0)
+            caret.c = wrapping ? 35 : 0; // 6*6-1
+          else if (caret.c > 35)
+            caret.c = wrapping ? 0 : 35;
+        }
+        else if (params.enable_gray24_colors && caret.r == num_16color_rows + num_rgb6_rows)
+        {
+          if (caret.c < 0)
+            caret.c = wrapping ? 23 : 0;
+          else if (caret.c > 23)
+            caret.c = wrapping ? 0 : 23;
+        }
       }
     }
+    
+    // TtD0123456789ABCDEF
+    // ###### ###### ###### ###### ###### ######
+    // ###### ###### ###### ###### ###### ######
+    // ###### ###### ###### ###### ###### ######
+    // ###### ###### ###### ###### ###### ######
+    // ###### ###### ###### ###### ###### ######
+    // ###### ###### ###### ###### ###### ######
+    // GGGGGGGGGGGGGGGGGGGGGGGG
     
     template<int NR, int NC>
     void draw(ScreenHandler<NR, NC>& sh, const RC& pos, int anim_ctr) const
     {
-      Color caret_fg_color = anim_ctr == 0 ? fg_color_sel : fg_color_sel_hilite;
-      Color caret_bg_color = get_color(caret);
-      if (!is_selected())
+      auto caret_bg_color = (params.enable_special_colors && caret.r == 0 && caret.c < -col_x_start) ? Color16::Black : get_color();
+      const auto caret_bg_color_inv = t8::get_contrast_color(caret_bg_color);
+      Color caret_fg_color_unsel = Color16::Default;
+      switch (params.fg_cursor_coloring)
       {
-        switch (fg_cursor_coloring)
-        {
-          case ColorPickerCursorColoring::BlackWhite:
-            caret_fg_color = t8::is_dark(caret_bg_color, true).value_or(false) ? Color16::White : Color16::Black;
-            break;
-          case ColorPickerCursorColoring::Contrast:
-            caret_fg_color = t8::get_contrast_color(caret_bg_color);
-            break;
-        }
+        case ColorPickerCursorColoring::BlackWhite:
+          caret_fg_color_unsel = t8::is_dark(caret_bg_color, true).value_or(false) ? Color16::White : Color16::Black;
+          break;
+        case ColorPickerCursorColoring::Contrast:
+          caret_fg_color_unsel = caret_bg_color_inv;
+          break;
       }
-      sh.write_buffer(std::string(1, select_char), pos.r, pos.c + caret, caret_fg_color, caret_bg_color);
+      if (params.enable_special_colors && caret.r == 0 && caret.c < -col_x_start)
+        caret_fg_color_unsel = params.special_color_fg_hilite_color;
+      auto caret_fg_color = anim_ctr == 0 ? (params.cursor_blinking == ColorPickerCursorBlinking::EveryOther ? caret_fg_color_unsel : fg_color_sel) : fg_color_sel_hilite;
+      
+      if (!is_selected())
+        caret_fg_color = caret_fg_color_unsel;
+      
+      // Cursor
+      if (!is_selected()
+          || params.cursor_blinking == ColorPickerCursorBlinking::Persist
+          || (anim_ctr == 0 && params.cursor_blinking == ColorPickerCursorBlinking::EveryOther))
+      {
+        char curr_char = select_char;
+        if (!is_selected() && params.enable_special_colors && caret.r == 0 && 0 <= caret.c && caret.c <= -col_x_start)
+        {
+          if (caret.c == 0)
+            curr_char = 'T';
+          else if (caret.c == 1)
+            curr_char = 't';
+          else if (caret.c == 2)
+            curr_char = 'D';
+        }
+        sh.write_buffer(std::string(1, curr_char), pos.r + caret.r, pos.c + caret.c, caret_fg_color, caret_bg_color);
+      }
+      
+      // Colors
+      for (int col = col_x_start; col <= col_x_end; ++col)
+      {
+        char curr_char = unselect_char;
+        if (col == -3)
+          curr_char = 'T';
+        else if (col == -2)
+          curr_char = 't';
+        else if (col == -1)
+          curr_char = 'D';
+        sh.write_buffer(std::string(1, curr_char), pos.r, pos.c + col - col_x_start, Color16::White, Color16::Black);
+      }
+      for (int col = col_4b_start; col <= col_4b_end; ++col)
+      {
+        char curr_char = unselect_char;
+        auto bg_color = get_color(col);
+        sh.write_buffer(std::string(1, curr_char), pos.r, pos.c + col - col_x_start, Color16::White, bg_color);
+      }
+      for (int col = col_rgb6_start; col <= col_rgb6_end; ++col)
+      {
+        char curr_char = unselect_char;
+        auto bg_color = get_color(col);
+        auto rgb6 = bg_color.try_get_rgb6();
+        if (!rgb6.has_value())
+          continue;
+        auto [r, g, b] = rgb6.value();
+        sh.write_buffer(std::string(1, curr_char), pos.r + (num_16color_rows + g), pos.c + (6*r + b), Color16::White, bg_color);
+      }
+      for (int col = col_g24_start; col <= col_g24_end; ++col)
+      {
+        char curr_char = unselect_char;
+        auto bg_color = get_color(col);
+        auto gray24 = bg_color.try_get_gray24();
+        if (!gray24.has_value())
+          continue;
+        auto [gray] = gray24.value();
+        auto row_offs = num_16color_rows + num_rgb6_rows;
+        sh.write_buffer(std::string(1, curr_char), pos.r + row_offs, pos.c + gray, Color16::White, bg_color);
+      }
+    }
     
-      for (int col = 0; col < c_num_colors; ++col)
-        sh.write_buffer(std::string(1, unselect_char), pos.r, pos.c + col, Color16::Transparent2, get_color(col));
+    int height() const
+    {
+      return num_rows;
     }
     
     Color get_color() const
     {
-      return get_color(caret);
+      if ((params.enable_special_colors || params.enable_4bit_colors)
+          && caret.r == 0)
+        return Color(caret.c + col_x_start); // 0 <= x <= 15+3 -> -3 <= x <= 15
+      if (params.enable_rgb6_colors && math::in_r_co(caret.r, num_16color_rows, num_16color_rows + num_rgb6_rows))
+      {
+        int r = caret.c / 6;
+        int g = caret.r - num_16color_rows;
+        int b = caret.c % 6;
+        return Color(r, g, b);
+      }
+      if (params.enable_gray24_colors && caret.r == num_16color_rows + num_rgb6_rows)
+        return Color(t8::Gray24(caret.c));
+      return Color16::Default;
     }
     
-    void set_color(Color color)
+    void set_color(Color color) // 254 (gray24:{22})
     {
       int idx = color.get_index();
-      if (idx < 0)
-        return;
-      caret = idx;
+      if (params.enable_special_colors && color.is_color16_special())
+      {
+        caret.c = idx - c_special_start;
+        caret.r = 0;
+      }
+      else if (params.enable_4bit_colors && color.is_color16_regular())
+      {
+        caret.c = idx - (params.enable_special_colors ? c_special_start : 0);
+        caret.r = 0;
+      }
+      else if (params.enable_rgb6_colors && color.is_rgb6())
+      {
+        auto [r, g, b] = color.try_get_rgb6().value();
+        caret.c = r * 6 + b;
+        caret.r = g + num_16color_rows;
+      }
+      else if (params.enable_gray24_colors && color.is_gray24())
+      {
+        auto [gray] = color.try_get_gray24().value();
+        caret.c = gray;
+        caret.r = num_16color_rows + num_rgb6_rows;
+      }
+      else
+      {
+        caret.c = 0;
+        caret.r = 0;
+      }
     }
     
     void clear()
     {
-      caret = 0;
+      caret = { 0, 0 };
     }
   };
 
