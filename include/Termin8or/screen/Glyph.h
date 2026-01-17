e0c19bf38f5cc9b4cde1ddc07d2f0b43fa9d0871
Rasmus Anthin <scratty@gmail.com>
2026-01-17 15:25:17 +0100

    Glyph.h: Adding asserts in Glyph(char32_t, char) for illegal combinations.

--- include/Termin8or/screen/Glyph.h
+++ include/Termin8or/screen/Glyph.h
@@ -23,7 +23,23 @@ namespace t8
     Glyph(char32_t pref, char fb = none)
       : preferred(pref)
       , fallback(fb)
-    {}
+    {
+      // 1) No fallback without preferred.
+      assert(!(preferred == none32 && fallback != none)
+             && "ERROR in Glyph(char32_t, char) : Fallback without preferred.");
+      
+      // 2) Fallback must be ASCII if present.
+      assert(!(fallback != none && static_cast<unsigned char>(fallback) > 0x7F)
+             && "ERROR in Glyph(char32_t, char) : Fallback must be ASCII (<=0x7F).");
+      
+      // 3) Preferred must be ASCII if fallback is not present.
+      assert(!(preferred != none32 && preferred > 0x7F && fallback == none)
+             && "ERROR in Glyph(char32_t, char) : Preferred cannot be non-ASCII while fallback is unset.");
+             
+      // Canonicalize ASCII glyphs.
+      if (preferred != none32 && preferred <= 0x7F)
+        fallback = static_cast<char>(preferred);
+    }
     
     bool operator==(const Glyph& g) const
     {
