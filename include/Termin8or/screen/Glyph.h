05e2285f8670029d34c2bd7c1dc944b585657ca8
Rasmus Anthin <scratty@gmail.com>
2026-01-17 15:28:10 +0100

    Glyph.h: Adding functions std::string str(bool legacy_ascii_only = false) and bool parse(const std::string& str, int& pos, bool legacy_ascii_only = false) and wrapper bool parse(const std::string& str, bool legacy_ascii_only = false). It took a while to flesh out the output and parsing rules, but I think this is pretty robust now.

--- include/Termin8or/screen/Glyph.h
+++ include/Termin8or/screen/Glyph.h
@@ -46,6 +46,183 @@ namespace t8
       return this->preferred == g.preferred
           && this->fallback == g.fallback;
     }
+    
+    // legacy_ascii_only == false:
+    // p == none32 && f == none => "[]"
+    // p != none32 && p <= 0x7F => "[<p>]"
+    // p != none32 && p > 0x7F  => "[<p_hex>,<f>]" (because of invariant assert 3)
+    //
+    // legacy_ascii_only == true:
+    // p == none32 && f == none => "?"
+    // p != none32 && p <= 0x7F => "<p>"
+    // p != none32 && p > 0x7F  => "<f>" (because of invariant assert 3)
+    //
+    // Round trips legacy_ascii_only == false:
+    // p == 'A' && f == none => "[A]"  => p == 'A', f == 'A'
+    // Round trips legacy_ascii_only == true:
+    // p == none32 && f == none => "?" => p == '?', f == '?'
+    // p == '?' && f == none => "?" => p == '?', f == '?'
+    std::string str(bool legacy_ascii_only = false) const
+    {
+      std::string ret;
+      auto fb_u = static_cast<unsigned char>(fallback);
+      if (legacy_ascii_only)
+      {
+        if (preferred == none32)
+          return "?";
+          
+        if (preferred <= 0x7F)
+          return std::string(1, static_cast<char>(preferred));
+          
+        assert(fallback != none && "Glyph::str(): fallback cannot be unset when preferred is > 0x7F");
+        assert(fb_u <= 0x7F && "Glyph::str(): fallback must be <= 0x7F");
+        return std::string(1, fallback);
+      }
+      
+      ret.push_back('[');
+      if (preferred != none32)
+      {
+        if (preferred <= 0x7F)
+          ret.push_back(static_cast<char>(preferred));
+        else
+        {
+          ret += str::int2hex(static_cast<uint32_t>(preferred));
+          
+          assert(fallback != none && "ERROR in Glyph::str() : Fallback character cannot be unset when preferred is > 0x7F!");
+          assert(fb_u <= 0x7F && "ERROR in Glyph::str() : Fallback character must be <= 0x7F!");
+          ret.push_back(',');
+          ret.push_back(fallback);
+        }
+      }
+      ret.push_back(']');
+      return ret;
+    }
+    
+    // legacy_ascii_only == false
+    // "[]" => p == none32, f == none
+    // "[A]" => p == A, f == none
+    // "[2603]" => error
+    // "[A,A]" => p == A, f == A (not outputed by str() though)
+    // "[A,2603]" => error
+    // "[2603,A]" => p == 2603, f == A
+    // "[2603,2603]" => error
+    //
+    // "?" => p == ?, f == ? (p == none32 && f == none outputs "?" though)
+    // "A" => p == A, f == A
+    bool parse(const std::string& str, int& pos, bool legacy_ascii_only = false)
+    {
+      // abcd[2603,e]fghi
+      // 0123456789ABCDEF
+      // pos = 4
+      // substr = [2603,e]fghi
+      //          0123456789AB
+      // end_idx = 7
+      // toks_str = substr.substr(0, end_idx + 1) = substr.substr(0, 8)
+      // toks_str = [2603,e]
+      //            01234567
+      
+      auto f_set_preferred = [&](const std::string& token)
+      {
+        auto tok_len = token.length();
+        if (tok_len == 1)
+          preferred = token[0];
+        else
+          preferred = str::hex2int(token);
+      };
+      
+      auto f_set_fallback = [&](const std::string& token)
+      {
+        auto tok_len = token.length();
+        if (tok_len == 1)
+          fallback = token[0];
+      };
+      
+      if (pos < 0 || static_cast<size_t>(pos) >= str.size())
+        return false;
+      auto substr = str.substr(pos);
+      if (legacy_ascii_only)
+      {
+        auto tok = std::string(1, substr[0]);
+        f_set_preferred(tok);
+        f_set_fallback(tok);
+        pos++;
+        return true;
+      }
+      
+      if (substr.starts_with("["))
+      {
+        auto end_idx = substr.find("]");
+        if (end_idx == 1)
+        {
+          pos += 2;
+          preferred = none32;
+          fallback = none;
+          return true;
+        }
+        if (end_idx == std::string::npos)
+        {
+          std::cerr << "ERROR in Glyph::parse() : Unable to find matching end bracket \"]\"!\n";
+          return false;
+        }
+        auto toks_len = end_idx + 1;
+        auto toks_str = substr.substr(0, toks_len);
+        auto tokens = str::tokenize(toks_str, { ',', '[', ']' });
+        //for (const auto& tok : tokens)
+        //  std::cout << tok << std::endl;
+        if (tokens.size() == 1)
+        {
+          const auto& tok0 = tokens[0];
+          if (tok0.length() > 1)
+          {
+            std::cerr << "ERROR in Glyph::parse() : A unicode code point > 0x7F must be followed by an ASCII (<= 0x7F) character!\n";
+            return false;
+          }
+          f_set_preferred(tok0);
+          f_set_fallback(tok0);
+          pos += toks_len;
+          return true;
+        }
+        else if (tokens.size() == 2)
+        {
+          f_set_preferred(tokens[0]);
+          if (preferred > 0x7F)
+          {
+            if (tokens[1].size() != 1)
+            {
+              std::cerr << "ERROR in Glyph::parse(): Fallback must be a single ASCII char.\n";
+              return false;
+            }
+            f_set_fallback(tokens[1]);
+            auto fb_u = static_cast<unsigned char>(fallback);
+            if (fallback == none || fb_u > 0x7F)
+            {
+              std::cerr << "ERROR in Glyph::parse(): Fallback must be ASCII (<=0x7F).\n";
+              return false;
+            }
+          }
+          else
+          {
+            // ASCII preferred; ignore provided fallback and normalize.
+            fallback = static_cast<char>(preferred);
+          }
+          pos += toks_len;
+          return true;
+        }
+        else
+        {
+          std::cerr << "ERROR in Glyph::parse() : Wrong number of tokens within bracketed glyph scope!\n";
+          return false;
+        }
+      }
+      std::cerr << "ERROR in Glyph::parse() : Unable to find a bracketed glyph scope!\n";
+      return false;
+    }
+    
+    bool parse(const std::string& str, bool legacy_ascii_only = false)
+    {
+      int pos = 0;
+      return parse(str, pos, legacy_ascii_only);
+    }
   };
   
 }
