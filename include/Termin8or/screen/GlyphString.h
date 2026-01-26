56983e6eaaeedb2f07c9dbd875e274c9494b4efc
Rasmus Anthin <scratty@gmail.com>
2026-01-26 08:53:33 +0100

    Moving struct GlyphString from ScreenHandler.h to new header file GlyphString.h. ... and making encode independent of ScreenHandler to avoid circular dependency. Instead we're using Glyph for the encoding which is more proper now.

---
+++ include/Termin8or/screen/GlyphString.h
@@ -0,0 +1,113 @@
+//
+//  GlyphString.h
+//  Termin8or
+//
+//  Created by Rasmus Anthin on 2026-01-26.
+//
+
+#pragma once
+#include "Glyph.h"
+#include <string_view>
+#include <string>
+#include <vector>
+
+
+namespace t8
+{
+
+  struct GlyphString
+  {
+    GlyphString() = default;
+    GlyphString(std::string_view sv)
+    {
+      glyph_vector.reserve(sv.size());
+      for (unsigned char c : sv)
+        glyph_vector.emplace_back(c, c);
+    }
+    GlyphString(const std::string& str)
+    {
+      glyph_vector.reserve(str.size());
+      for (unsigned char c : str)
+        glyph_vector.emplace_back(c, c);
+    }
+    GlyphString(const Glyph& glyph)
+    {
+      glyph_vector.emplace_back(glyph);
+    }
+    
+    template<typename CharT>
+    std::string encode() const
+    {
+      std::string str;
+      for (const auto& g : glyph_vector)
+        str += g.encode_single_width_glyph<CharT>();
+      return str;
+    }
+    
+    Glyph& operator[](int idx) { return glyph_vector[idx]; }
+    const Glyph& operator[](int idx) const { return glyph_vector[idx]; }
+  
+    std::vector<Glyph> glyph_vector;
+  };
+  
+  namespace literals
+  {
+    // "_gs" produces an ASCII part (no CharT, no ScreenHandler needed).
+    inline GlyphString operator"" _gs(const char* s, size_t n)
+    {
+      GlyphString gs;
+      gs.glyph_vector.reserve(n);
+      for (size_t i = 0; i < n; ++i)
+      {
+        auto b = static_cast<unsigned char>(s[i]);
+        gs.glyph_vector.emplace_back(b, b);
+      }
+      return gs;
+    }
+  }
+  
+  namespace operators
+  {
+    inline GlyphString operator+(GlyphString lhs, const GlyphString& rhs)
+    {
+      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + rhs.glyph_vector.size());
+      for (const auto& g : rhs.glyph_vector)
+        lhs.glyph_vector.emplace_back(g);
+      return lhs;
+    }
+  
+    // Append ASCII.
+    inline GlyphString operator+(GlyphString lhs, std::string_view rhs)
+    {
+      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + rhs.size());
+      for (unsigned char c : rhs)
+        lhs.glyph_vector.emplace_back(c, c);
+      return lhs;
+    }
+    
+    // Append char.
+    inline GlyphString operator+(GlyphString lhs, char rhs)
+    {
+      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + 1);
+      lhs.glyph_vector.emplace_back(rhs, rhs);
+      return lhs;
+    }
+    
+    // Append char32_t.
+    inline GlyphString operator+(GlyphString lhs, char32_t rhs)
+    {
+      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + 1);
+      lhs.glyph_vector.emplace_back(rhs, rhs);
+      return lhs;
+    }
+    
+    // Append Glyph.
+    inline GlyphString operator+(GlyphString lhs, const Glyph& rhs)
+    {
+      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + 1);
+      lhs.glyph_vector.emplace_back(rhs);
+      return lhs;
+    }
+  }
+
+}
--- include/Termin8or/screen/ScreenHandler.h
+++ include/Termin8or/screen/ScreenHandler.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Text.h"
 #include "Styles.h"
+#include "GlyphString.h"
 #include "../geom/Rectangle.h"
 #include "../geom/RC.h"
 #include "../drawing/Texture.h"
@@ -29,101 +30,6 @@ namespace t8x::screen_scaling
 
 namespace t8
 {
-
-  struct GlyphString
-  {
-    GlyphString() = default;
-    GlyphString(std::string_view sv)
-    {
-      glyph_vector.reserve(sv.size());
-      for (unsigned char c : sv)
-        glyph_vector.emplace_back(c, c);
-    }
-    GlyphString(const std::string& str)
-    {
-      glyph_vector.reserve(str.size());
-      for (unsigned char c : str)
-        glyph_vector.emplace_back(c, c);
-    }
-    GlyphString(const Glyph& glyph)
-    {
-      glyph_vector.emplace_back(glyph);
-    }
-    
-    template<int NR, int NC, typename CharT>
-    std::string encode(ScreenHandler<NR, NC, CharT>& sh) const
-    {
-      std::string str;
-      for (const auto& g : glyph_vector)
-        str += sh.encode_single_width_glyph(g);
-      return str;
-    }
-    
-    Glyph& operator[](int idx) { return glyph_vector[idx]; }
-    const Glyph& operator[](int idx) const { return glyph_vector[idx]; }
-  
-    std::vector<Glyph> glyph_vector;
-  };
-  
-  namespace literals
-  {
-    // "_gs" produces an ASCII part (no CharT, no ScreenHandler needed).
-    inline GlyphString operator"" _gs(const char* s, size_t n)
-    {
-      GlyphString gs;
-      gs.glyph_vector.reserve(n);
-      for (size_t i = 0; i < n; ++i)
-      {
-        auto b = static_cast<unsigned char>(s[i]);
-        gs.glyph_vector.emplace_back(b, b);
-      }
-      return gs;
-    }
-  }
-  
-  namespace operators
-  {
-    inline GlyphString operator+(GlyphString lhs, const GlyphString& rhs)
-    {
-      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + rhs.glyph_vector.size());
-      for (const auto& g : rhs.glyph_vector)
-        lhs.glyph_vector.emplace_back(g);
-      return lhs;
-    }
-  
-    // Append ASCII.
-    inline GlyphString operator+(GlyphString lhs, std::string_view rhs)
-    {
-      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + rhs.size());
-      for (unsigned char c : rhs)
-        lhs.glyph_vector.emplace_back(c, c);
-      return lhs;
-    }
-    
-    // Append char.
-    inline GlyphString operator+(GlyphString lhs, char rhs)
-    {
-      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + 1);
-      lhs.glyph_vector.emplace_back(rhs, rhs);
-      return lhs;
-    }
-    
-    // Append char32_t.
-    inline GlyphString operator+(GlyphString lhs, char32_t rhs)
-    {
-      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + 1);
-      lhs.glyph_vector.emplace_back(rhs, rhs);
-      return lhs;
-    }
-    
-    // Append Glyph.
-    inline GlyphString operator+(GlyphString lhs, const Glyph& rhs)
-    {
-      lhs.glyph_vector.reserve(lhs.glyph_vector.size() + 1);
-      lhs.glyph_vector.emplace_back(rhs);
-      return lhs;
-    }
-  }
 
   struct OrderedText
   {
@@ -353,7 +259,7 @@ namespace t8
     
     void write_buffer(const GlyphString& gstr, int r, int c, Color fg_color, Color bg_color = Color16::Transparent)
     {
-      write_buffer(gstr.encode(*this), r, c, fg_color, bg_color);
+      write_buffer(gstr.encode<CharT>(), r, c, fg_color, bg_color);
     }
     
     void write_buffer(const std::string& str, const RC& pos, const Style& style)
