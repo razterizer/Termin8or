68b302c95018c37a60f54e2e5b93911185859a63
Rasmus Anthin <scratty@gmail.com>
2026-02-07 10:01:07 +0100

    ScreenHandler.h: write_buffer() overload that takes a str::StringBox now takes a template argument StrT, so it should work again.

--- include/Termin8or/screen/ScreenHandler.h
+++ include/Termin8or/screen/ScreenHandler.h
@@ -201,7 +201,8 @@ namespace t8
     }
     
     // write_buffer using StringBox.
-    void write_buffer(const str::StringBox& sb, int r, int c, Color fg_color, Color bg_color = Color16::Transparent)
+    template<typename StrT>
+    void write_buffer(const str::StringBox<StrT>& sb, int r, int c, Color fg_color, Color bg_color = Color16::Transparent)
     {
       auto Nr = static_cast<int>(sb.text_lines.size());
       for (int r_idx = 0; r_idx < Nr; ++r_idx)
