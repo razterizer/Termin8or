0d721492aad5fb99c8003bb53349c4b717808f18
Rasmus Anthin <scratty@gmail.com>
2026-02-18 11:05:21 +0100

    ScreenHandler.h: In init_terminal_mode() we set the windows console code page to 437 after the terminal mode has been initialized and if non_vt_console() = true && CharT=char32_t && WIN_NON_VT_TRY_CP437 is set (Windows only).

--- include/Termin8or/screen/ScreenHandler.h
+++ include/Termin8or/screen/ScreenHandler.h
@@ -220,6 +220,13 @@ namespace t8
     void init_terminal_mode()
     {
       m_text->init_terminal_mode();
+      
+#ifdef _WIN32
+      if (m_text->non_vt_console())
+        if constexpr (std::is_same_v<CharT, char32_t>)
+          if (m_text->get_glyph_mapping_policy() == GlyphMappingPolicy::WIN_NON_VT_TRY_CP437)
+            SetConsoleOutputCP(437);
+#endif
     }
     
     inline std::string encode_single_width_glyph(char32_t preferred,
