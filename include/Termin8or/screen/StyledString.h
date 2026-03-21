4c50d984d1fb8d4e2df7c00d2b90e3800c2ead44
Rasmus Anthin <scratty@gmail.com>
2026-03-21 02:10:39 +0100

    Adding new header StyledString.h which just contain a struct StyledString that contain an std::string and a Style.

---
+++ include/Termin8or/screen/StyledString.h
@@ -0,0 +1,22 @@
+//
+//  StyledString.h
+//  Termin8or
+//
+//  Created by Rasmus Anthin on 2026-03-21.
+//
+
+#pragma once
+#include "Styles.h"
+#include <string>
+
+
+namespace t8
+{
+  
+  struct StyledString
+  {
+    std::string text;
+    Style style;
+  };
+  
+}
