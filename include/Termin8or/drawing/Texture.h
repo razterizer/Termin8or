48586d6145ea952df4bad50ddd0a39ee4d90e777
Rasmus Anthin <scratty@gmail.com>
2026-05-13 22:28:28 +0200

    Texture::load_ansi(): Preserving low CP437 bytes during decoding.

--- include/Termin8or/drawing/Texture.h
+++ include/Termin8or/drawing/Texture.h
@@ -1314,16 +1314,19 @@ namespace t8
           bool decoded = false;
           
           unsigned char b = static_cast<unsigned char>(line[i]);
-          if (b < 0x20)
+          if (glyph_encoding == AnsiGlyphEncoding::UTF8)
           {
-            ch32 = U' ';
-            ++i;
-            decoded = true;
-          }
-          else if (glyph_encoding == AnsiGlyphEncoding::UTF8)
-          {
-            decoded = utf8::decode_next_utf8_char32(line, ch32, byte_idx);
-            i = static_cast<int>(byte_idx);
+            if (b < 0x20)
+            {
+              ch32 = U' ';
+              ++i;
+              decoded = true;
+            }
+            else
+            {
+              decoded = utf8::decode_next_utf8_char32(line, ch32, byte_idx);
+              i = static_cast<int>(byte_idx);
+            }
           }
           else if (glyph_encoding == AnsiGlyphEncoding::CP437)
           {
