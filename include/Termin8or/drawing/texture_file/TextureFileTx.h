1e4ad04b11ea842740d3e2b4d5d87325a5d769c1
Rasmus Anthin <scratty@gmail.com>
2026-05-15 13:56:17 +0200

    TextureFileTx.h: Canonizing error and warning messages.

--- include/Termin8or/drawing/texture_file/TextureFileTx.h
+++ include/Termin8or/drawing/texture_file/TextureFileTx.h
@@ -119,7 +119,7 @@ namespace t8
                   tex.ver = ver_parsed;
                 else
                 {
-                  std::cerr << "ERROR in Texture::load_tx() : Incompatible texture version: version = " + tokens[1] << '\n';
+                  std::cerr << "ERROR in TextureFileAnsi::load_tx() : Incompatible texture version: version = " + tokens[1] << '\n';
                   return false;
                 }
               }
@@ -145,7 +145,7 @@ namespace t8
           }
           else if (r > 2)
           {
-            std::cerr << "ERROR in Texture::load_tx() : Incorrect number of header lines.\n";
+            std::cerr << "ERROR in TextureFileAnsi::load_tx() : Incorrect number of header lines.\n";
           }
         }
         else if (section == 1)
