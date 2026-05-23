056cf70cd5c8490f25e808e8fa27dc76baa57a1d
Rasmus Anthin <scratty@gmail.com>
2026-05-23 22:59:23 +0200

    ascii_helpers.h: Adding new function is_ascii().

--- include/Termin8or/str/ascii_helpers.h
+++ include/Termin8or/str/ascii_helpers.h
@@ -16,5 +16,11 @@ namespace t8
   {
     return math::in_r_c<CharT>(cp, 0x20, 0x7E);
   }
+  
+  template<typename CharT>
+  inline constexpr bool is_ascii(CharT cp) noexcept
+  {
+    return math::in_r_c<CharT>(cp, 0x00, 0x7F);
+  }
 
 }
